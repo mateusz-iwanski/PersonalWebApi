dc21390bda2ea4c2b4189c7bfdba02b5e6b4edbb
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-16 21:46:44 +0100

    Add UserSeeder to initialize admin user and roles
    Introduced UserSeeder class in PersonalWebApi.Seeder namespace.
    The seeder initializes the database with essential data, including
    an administrator account and basic user roles. It reads admin
    credentials from appsettings.json and ensures idempotency by
    checking for existing roles and users before adding new entries.

---
+++ Seeder/UserSeeder.cs
@@ -0,0 +1,53 @@
+﻿using PersonalWebApi.Entities;
+
+namespace PersonalWebApi.Seeder
+{
+    public class UserSeeder
+    {
+        private readonly PersonalWebApiDbContext _context;
+
+        public UserSeeder(PersonalWebApiDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        /// <summary>
+        /// Check if basic customer roles exist in the database and add them if they don't
+        /// </summary>
+        public void SeedBasic()
+        {
+            if (_context.Users.Count() == 0)
+            {
+
+                var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                string? name = configuration.GetSection("UserSettings:Administrator:Username").Value ??
+                                throw new FileLoadException("Can't read UserSettings:Administrator:Username settings from settings.json");
+
+                string? email = configuration.GetSection("UserSettings:Administrator:Email").Value ??
+                                throw new FileLoadException("Can't read UserSettings:Administrator:Email settings from settings.json");
+
+                string? passwordHash = configuration.GetSection("UserSettings:Administrator:PasswordHash").Value ??
+                                throw new FileLoadException("Can't read UserSettings:Administrator:PasswordHash settings from settings.json");
+
+                var roleId = _context.Roles.FirstOrDefault(a => a.Name == "Administrator").Id; // Role seeder should be run before user seeder
+
+                if (_context.Users.FirstOrDefault(a => a.Name == "Administrator") == null)
+                    _context.Users.Add(
+                        new User
+                        {
+                            Name = name,
+                            Email = email,
+                            PasswordHash = passwordHash,
+                            RoleId = roleId
+                        }
+                    );
+
+                _context.SaveChanges();
+            }
+        }
+    }
+}
