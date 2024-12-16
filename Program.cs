bbe46d30ed85737effa71cc966d4d55e82bf7db1
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-16 14:36:14 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+
+using Microsoft.OpenApi.Models;
+
+namespace PersonalWebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //var builder = WebApplication.CreateBuilder(args);
+
+            //// Add services to the container.
+
+            //builder.Services.AddControllers();
+            //// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            //builder.Services.AddOpenApi();
+
+            //var app = builder.Build();
+
+            //// Configure the HTTP request pipeline.
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.MapOpenApi();
+            //}
+
+            //app.UseHttpsRedirection();
+
+            //app.UseAuthorization();
+
+
+            //app.MapControllers();
+
+            //app.Run();
+
+
+            var builder = WebApplication.CreateBuilder(args);
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                    c.RoutePrefix = "swagger";
+                });
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            app.Run();
+
+        }
+    }
+}
