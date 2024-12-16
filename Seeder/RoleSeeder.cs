040480159f9437681c61d34a3a2c30311dcec65d
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-16 21:46:29 +0100

    Add RoleSeeder class to seed basic roles in the database
    A new RoleSeeder class has been introduced in the PersonalWebApi.Seeder namespace to handle the seeding of basic roles. The class uses PersonalWebApiDbContext for database operations. The SeedBasic method ensures that "Administrator" and "User" roles are present in the database, adding them if they are missing. The method reads configuration settings from appsettings.json, although this configuration is not utilized in the current implementation. The roles are added to the database and saved using _context.SaveChanges().

---
+++ Seeder/RoleSeeder.cs
@@ -0,0 +1,47 @@
+﻿using PersonalWebApi.Entities;
+
+namespace PersonalWebApi.Seeder
+{
+    public class RoleSeeder
+    {
+        private readonly PersonalWebApiDbContext _context;
+
+        public RoleSeeder(PersonalWebApiDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Check if basic customer roles exist in the database and add them if they don't
+        /// </summary>
+        public void SeedBasic()
+        {
+            if (_context.Roles.Count() == 0)
+            {
+
+                var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                if (_context.Roles.FirstOrDefault(a => a.Name == "Administrator") == null)
+                    _context.Roles.Add(
+                        new Role
+                        {
+                            Name = "Administrator"
+                        }
+                    );
+
+                if (_context.Roles.FirstOrDefault(a => a.Name == "User") == null)
+                    _context.Roles.Add(
+                        new Role
+                        {
+                            Name = "User"
+                        }
+                    );
+
+                _context.SaveChanges();
+            }
+        }
+    }
+}
