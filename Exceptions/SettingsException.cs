b51c87f5a012216807e01948de530a5797c5c205
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-16 22:17:24 +0100

    Add SettingsException class to handle settings-related errors
    A new namespace `PersonalWebApi.Exceptions` has been introduced.
    Within this namespace, a new class `SettingsException` has been added.
    The `SettingsException` class inherits from `CustomException`.
    Two constructors have been defined for the `SettingsException` class:
    - One that takes a single `string` parameter `message` and passes it to the base class constructor.
    - Another that takes a `string` parameter `message` and an `Exception` parameter `inner`, and passes both to the base class constructor.

---
+++ Exceptions/SettingsException.cs
@@ -0,0 +1,13 @@
+﻿namespace PersonalWebApi.Exceptions
+{
+    public class SettingsException : CustomException
+    {
+        public SettingsException(string message) : base(message)
+        {
+        }
+
+        public SettingsException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
