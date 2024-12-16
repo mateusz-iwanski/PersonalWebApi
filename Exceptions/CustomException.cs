47a64c0a29f9c7cac232b8cb9efcce8ffe47e044
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-16 22:17:11 +0100

    Add CustomException class with enhanced logging
    Introduce a new CustomException class in the PersonalWebApi.Exceptions namespace. This class extends Exception and includes additional functionality for logging exceptions using the ILogger interface from Microsoft.Extensions.Logging.
    
    The class includes two constructors that utilize CallerFilePath, CallerLineNumber, and CallerMemberName attributes to provide detailed information about where the exception was thrown. A static constructor initializes the _logger field with a LoggerFactory set to a minimum log level of Trace.
    
    The LogException method logs the exception details, including the message, file path, line number, and member name. If an inner exception is provided, it is also logged.

---
+++ Exceptions/CustomException.cs
@@ -0,0 +1,62 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Logging;
+
+namespace PersonalWebApi.Exceptions
+{
+    public class CustomException : Exception
+    {
+        private static readonly ILogger<CustomException> _logger;
+
+        static CustomException()
+        {
+            var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.ClearProviders();
+                builder.SetMinimumLevel(LogLevel.Trace);
+            });
+            _logger = loggerFactory.CreateLogger<CustomException>();
+        }
+
+        public CustomException(
+            string message,
+            [CallerFilePath] string filePath = "",
+            [CallerLineNumber] int lineNumber = 0,
+            [CallerMemberName] string memberName = ""
+            ) 
+            : base($"File: {filePath}|Line: {lineNumber}|Member: {memberName}|message: {message}")
+        {
+            LogException(message, null, filePath, lineNumber, memberName);
+        }
+
+        public CustomException(
+            string message, 
+            Exception inner,
+            [CallerFilePath] string filePath = "",
+            [CallerLineNumber] int lineNumber = 0,
+            [CallerMemberName] string memberName = ""
+            )
+            : base($"{message} (File: {filePath}, Line: {lineNumber}, Member: {memberName})", inner)
+        {
+            LogException(message, inner, filePath, lineNumber, memberName);
+        }
+
+        private void LogException(
+            string message, 
+            Exception? innerException, 
+            string filePath, 
+            int lineNumber, 
+            string memberName
+            )
+        {
+            var logMessage = $"{message} (File: {filePath}, Line: {lineNumber}, Member: {memberName})";
+            if (innerException != null)
+            {
+                _logger.LogError(innerException, logMessage);
+            }
+            else
+            {
+                _logger.LogError(logMessage);
+            }
+        }
+    }
+}
