b7486576ae60bfcf71b53d337be697bb0e689860
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-17 14:59:27 +0100

    Add XML documentation to RoleCreateDtoValidator class
    Added XML documentation comments to the RoleCreateDtoValidator class and its constructor in RoleCreateDtoValidator.cs. This includes a summary description for the class and its constructor, as well as a parameter description for the constructor's context parameter. These changes improve code readability and aid in generating documentation.

--- Validations/System/RoleCreateDtoValidator.cs
+++ Validations/System/RoleCreateDtoValidator.cs
@@ -4,10 +4,17 @@ using PersonalWebApi.Models.System;
 
 namespace PersonalWebApi.Validations.System
 {
+    /// <summary>
+    /// Validator for RoleCreateDto.
+    /// </summary>
     public class RoleCreateDtoValidator : AbstractValidator<RoleCreateDto>
     {
         private readonly PersonalWebApiDbContext _context;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RoleCreateDtoValidator"/> class.
+        /// </summary>
+        /// <param name="context">The database context.</param>
         public RoleCreateDtoValidator(PersonalWebApiDbContext context)
         {
             _context = context;
