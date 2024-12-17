78b4dd76e075ee163ad9ef12bfc56242c9fbb148
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-17 14:51:36 +0100

    Add RoleCreateDtoValidator with validation rules
    Added necessary using directives for FluentValidation and relevant namespaces. Introduced a new namespace `PersonalWebApi.Validations.System` and created `RoleCreateDtoValidator` class inheriting from `AbstractValidator<RoleCreateDto>`. Added a private readonly field `_context` of type `PersonalWebApiDbContext` and implemented a constructor to initialize it. Added validation rules for the `Name` property of `RoleCreateDto` to ensure it is not empty, does not exceed 20 characters, and is unique in the `Roles` table with a custom error message for duplicates.

---
+++ Validations/System/RoleCreateDtoValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using PersonalWebApi.Entities.System;
+using PersonalWebApi.Models.System;
+
+namespace PersonalWebApi.Validations.System
+{
+    public class RoleCreateDtoValidator : AbstractValidator<RoleCreateDto>
+    {
+        private readonly PersonalWebApiDbContext _context;
+
+        public RoleCreateDtoValidator(PersonalWebApiDbContext context)
+        {
+            _context = context;
+
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(20)
+                .Must((name) =>
+                {
+                    return !_context.Roles.Any(c => c.Name.Trim() == name.Trim());
+                })
+                .WithMessage("The role already exists.");
+        }
+    }
+}
