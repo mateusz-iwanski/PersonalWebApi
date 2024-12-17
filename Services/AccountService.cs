0af988c169cd7061606a381d4b6846c3037a9025
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-17 11:42:15 +0100

    Add AccountService with user management methods
    The `AccountService` class has been added to the `PersonalWebApi.Services` namespace, implementing the `IAccountService` interface. It includes methods for user management such as `DeleteUserAsync`, `GetAllUsersAsync`, and `RegisterUserAsync`. The class has dependencies on `PersonalWebApiDbContext` and `IPasswordHasher<User>`.
    
    Additionally, the `ChangeAdminPassword` method has been added to change the administrator's password, with verification against a value stored in `appsettings.json`. This method throws a `SettingsException` if the verification value cannot be read and returns `false` if the provided verification password does not match the one in the settings file. If verification is successful, it updates the password hash of the administrator.

---
+++ Services/AccountService.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PersonalWebApi.Entities;
+using PersonalWebApi.Exceptions;
+using PersonalWebApi.Models;
+
+namespace PersonalWebApi.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly PersonalWebApiDbContext _context;
+        private readonly IPasswordHasher<User> _passwordHasher;
+
+        public AccountService(PersonalWebApiDbContext context, IPasswordHasher<User> passwordHasher)
+        {
+            _context = context;
+            _passwordHasher = passwordHasher;
+        }
+
+        public async Task<bool> DeleteUserAsync(int id)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.id == id);
+            if (user == null)
+                return false;
+            
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+
+            return true;
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            return _context.Users.ToList();
+        }
+
+        public async Task RegisterUserAsync(RegisterUserDto registerUserDto)
+        {
+            var newUser = new User
+            {
+                Email = registerUserDto.Email,
+                Name = registerUserDto.Name,
+                RoleId = registerUserDto.RoleId,
+            };
+
+            newUser.PasswordHash = _passwordHasher.HashPassword(newUser, registerUserDto.Password);
+
+            _context.Users.Add(newUser);
+            _context.SaveChanges();
+        }
+
+        public async Task<bool> ChangeAdminPassword(String newPassword, String passwordVerification)
+        {
+            var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+            // get the password from the settings.json file
+            string? passVerification = configuration.GetSection("UserSettings:Administrator:PasswordVerification").Value ??
+                throw new SettingsException("Can't read UserSettings:Administrator:PasswordVerification settings from settings.json");
+
+            if (passVerification != passwordVerification)
+                return false;
+
+            var admin = _context.Users.FirstOrDefault(u => u.Role.Name == "Administrator");
+
+            admin.PasswordHash = _passwordHasher.HashPassword(admin, newPassword);
+
+            return false;
+        }
+    }
+}
