9b9a5fd190e5652f3dfb3e466894f8238ad7f481
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-17 11:42:29 +0100

    Add IAccountService interface for user management
    Introduced a new interface `IAccountService` in the `PersonalWebApi.Services` namespace. This interface includes methods for deleting a user, retrieving all users, registering a new user, and changing the admin password. Added necessary using directives for `PersonalWebApi.Entities` and `PersonalWebApi.Models`.

---
+++ Services/IAccountService.cs
@@ -0,0 +1,13 @@
+﻿using PersonalWebApi.Entities;
+using PersonalWebApi.Models;
+
+namespace PersonalWebApi.Services
+{
+    public interface IAccountService
+    {
+        Task<bool> DeleteUserAsync(int id);
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task RegisterUserAsync(RegisterUserDto registerUserDto);
+        Task<bool> ChangeAdminPassword(String newPassword, String passwordVerification);
+    }
+}
