09d8a037911e8063c3ef08aaa7fa587066257d77
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-17 14:51:22 +0100

    Add RoleId validation to RegisterUserDtoValidator
    - Updated RegisterUserDtoValidator to depend on PersonalWebApiDbContext for RoleId validation.
    - Added using directives for PersonalWebApi.Entities.System and PersonalWebApi.Models.System.
    - Changed namespace to PersonalWebApi.Validations.System.
    - Added validation rule to check if RoleId exists in the database.
    - Updated error message for RoleId validation to "The user role does not exist."

---
+++ Validations/System/RegisterUserDtoValidator.cs
@@ -0,0 +1,32 @@
+﻿
+using FluentValidation;
+using PersonalWebApi.Entities.System;
+using PersonalWebApi.Models.System;
+
+namespace PersonalWebApi.Validations.System
+{
+    public class RegisterUserDtoValidator : AbstractValidator<RegisterUserDto>
+    {
+        private readonly PersonalWebApiDbContext _context;
+
+        public RegisterUserDtoValidator(PersonalWebApiDbContext context)
+        {
+            _context = context;
+
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Password).NotEmpty().MinimumLength(8);
+            RuleFor(x => x.RoleId)
+                .NotEmpty()
+                .Must(roleId => roleId != 1)
+                    .WithMessage("RoleId cannot be 1 because it is an administrator role.")
+                .Must((roleId) =>
+                {
+                    return _context.Roles.Any(c => c.Id == roleId);
+                })
+                .WithMessage("The user role does not exist.");
+
+        }
+    }
+
+}
