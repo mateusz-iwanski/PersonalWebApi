20fafced56e7dd5c29bcba4f992aa1b9a5ed988d
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-17 11:41:45 +0100

    Add RegisterUserDto class for user registration
    Introduce RegisterUserDto class in PersonalWebApi.Models namespace.
    This DTO includes Email, Name, Password, and RoleId properties,
    all marked with appropriate validation attributes to ensure
    required fields are provided and validated during user registration.

---
+++ Models/RegisterUserDto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using PersonalWebApi.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalWebApi.Models
+{
+    public class RegisterUserDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        public string Password { get; set; }
+
+        [Required]
+        public int RoleId { get; set; }
+    }
+}
