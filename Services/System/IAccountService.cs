c069d60d9b478f15c1c3fd207635132fbeef5dab
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-18 14:19:50 +0100

    Refactor IAccountService interface methods
    Changed return type of ChangeUserPasswordAsync, DeleteRoleAsync, DeleteUserAsync, and ChangeAdminPasswordAsync from Task<bool> to Task. Added GenerateJwt method to IAccountService, which takes a LoginDto object and returns a string.

--- Services/System/IAccountService.cs
+++ Services/System/IAccountService.cs
@@ -5,13 +5,14 @@ namespace PersonalWebApi.Services.System
 {
     public interface IAccountService
     {
-        Task<bool> ChangeUserPasswordAsync(int userId, string newPassword);
+        Task ChangeUserPasswordAsync(int userId, string newPassword);
         Task AddRolesAsync(RoleCreateDto role);
         Task<IEnumerable<Role>> GetAllRolesAsync();
-        Task<bool> DeleteRoleAsync(int id);
-        Task<bool> DeleteUserAsync(int id);
+        Task DeleteRoleAsync(int id);
+        Task DeleteUserAsync(int id);
         Task<IEnumerable<User>> GetAllUsersAsync();
         Task RegisterUserAsync(RegisterUserDto registerUserDto);
-        Task<bool> ChangeAdminPasswordAsync(string newPassword, string passwordVerification);
+        Task ChangeAdminPasswordAsync(string newPassword, string passwordVerification);
+        string GenerateJwt(LoginDto loginDto);
     }
 }
