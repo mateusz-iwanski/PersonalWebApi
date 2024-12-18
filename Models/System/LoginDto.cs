5effaa19aeb3c4313cfa9b303bed574d2550e988
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-18 14:19:23 +0100

    Add LoginDto class for user login information
    Introduce LoginDto class in PersonalWebApi.Models.System namespace.
    This class serves as a Data Transfer Object (DTO) for user login
    information in an API. It includes two required properties: Email
    (validated for correct email format) and Password. The class and
    its properties are documented with XML comments.

---
+++ Models/System/LoginDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalWebApi.Models.System
+{
+    /// <summary>
+    /// User login Data Transfer Object for API
+    /// </summary>
+    public class LoginDto
+    {
+        /// <summary>
+        /// User email 
+        /// </summary>
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// User password
+        /// </summary>
+        [Required]
+        public string Password { get; set; }
+    }
+}
