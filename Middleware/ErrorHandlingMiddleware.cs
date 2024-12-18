67a42cfe6f09ead0f5d8ba2a4010f145f1550771
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-18 14:19:13 +0100

    Add ErrorHandlingMiddleware for centralized exception handling
    Introduced a new `ErrorHandlingMiddleware` class in the `PersonalWebApi.Middleware` namespace to handle exceptions during HTTP request processing. The middleware uses dependency injection to obtain an `ILogger<ErrorHandlingMiddleware>` for logging and an `IHostEnvironment` to determine the environment. The `InvokeAsync` method catches specific exceptions (`BadRequestException`, `NotFoundException`, `UnauthorizedAccessException`, `SettingsException`) and handles them with appropriate status codes. General exceptions are logged, and responses are tailored based on the environment. The `HandleExceptionAsync` method constructs and logs detailed error responses, which are serialized to JSON using `Newtonsoft.Json`. An `ErrorResponse` class structures the error response.

---
+++ Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,90 @@
+﻿using Newtonsoft.Json;
+using PersonalWebApi.Exceptions;
+using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace PersonalWebApi.Middleware
+{
+    public class ErrorHandlingMiddleware : IMiddleware
+    {
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+
+        public ErrorHandlingMiddleware(ILogger<ErrorHandlingMiddleware> logger, IHostEnvironment env)
+        {
+            _logger = logger;
+            _env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch (BadRequestException exception)
+            {
+                await HandleExceptionAsync(context, exception, 400);
+            }
+            catch (NotFoundException exception)
+            {
+                await HandleExceptionAsync(context, exception, 404);
+            }
+            catch (UnauthorizedAccessException exception)
+            {
+                await HandleExceptionAsync(context, exception, 401);
+            }
+            catch (SettingsException exception)
+            {
+                await HandleExceptionAsync(context, exception, 500);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                if (Debugger.IsAttached)
+                {
+                    await context.Response.WriteAsync(ex.Message);
+                    _logger.LogDebug($"REQUEST ERROR ==== {ex.Message}");
+                }
+                else
+                {
+                    context.Response.StatusCode = 500;  // internal server error
+                    await context.Response.WriteAsync("Something went wrong");
+                }
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception, int statusCode)
+        {
+            var correlationId = Guid.NewGuid().ToString();
+
+            var response = context.Response;
+            response.ContentType = "application/json";
+            response.StatusCode = statusCode;
+
+            var errorResponse = new ErrorResponse
+            {
+                StatusCode = statusCode,
+                Message = exception.Message,
+                Detail = _env.IsDevelopment() ? exception.StackTrace : null
+            };
+
+            var logErrorBuild = new StringBuilder()
+                .Append($"Error {correlationId}|")
+                .Append($"Processing request for {context.Request.Path}|")
+                .Append($"Response: {JsonConvert.SerializeObject(errorResponse)}");
+
+            _logger.LogError(exception, logErrorBuild.ToString());
+
+            await response.WriteAsync(JsonConvert.SerializeObject(errorResponse));
+        }
+    }
+
+    public class ErrorResponse
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public string Detail { get; set; }
+    }
+}
