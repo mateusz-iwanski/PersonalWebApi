0aeb006a06d0a61be1f096994d1cd66e6ba66490
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-22 22:17:13 +0100

    Add IBlobStorageService interface for Azure blob operations
    Introduced a new namespace `PersonalWebApi.Services.Azure` and
    added the `IBlobStorageService` interface. This interface includes
    methods for uploading and deleting files in temporary and library
    storage, both from local files and from URIs. It also supports
    options for overwriting files and setting TTL for temporary files.

---
+++ Services/Azure/IBlobStorageService.cs
@@ -0,0 +1,13 @@
+﻿
+namespace PersonalWebApi.Services.Azure
+{
+    public interface IBlobStorageService
+    {
+        Task<Uri> UploadToTempAsync(IFormFile file, double ttlInDays, bool overwrite = true);
+        Task<Uri> UploadToLibrary(IFormFile file, bool overwrite = false);
+        Task DeleteFileFromTemp(string fileName);
+        Task DeleteFileFromLibrary(string fileName);
+        Task<Uri> UploadFromUriToTemp(string fileUri, string fileName, double ttlInDays, bool overwrite = false);
+        Task<Uri> UploadFromUriToLibrary(string fileUri, string fileName, bool overwrite = false);
+    }
+}
