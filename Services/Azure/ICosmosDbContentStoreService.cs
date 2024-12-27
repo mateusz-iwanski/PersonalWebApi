5f5556605712838390924a786773f59d02d0fb10
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-27 16:51:48 +0100

    Add ICosmosDbContentStoreService interface for Cosmos DB ops
    Introduce ICosmosDbContentStoreService in PersonalWebApi.Services.Azure.
    This interface defines async methods for CRUD operations and querying
    items in Cosmos DB, using ItemResponse<SiteContentStoreCosmosDbDto> for
    return values.

---
+++ Services/Azure/ICosmosDbContentStoreService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Cosmos;
+using PersonalWebApi.Models.Azure;
+
+namespace PersonalWebApi.Services.Azure
+{
+    public interface ICosmosDbContentStoreService
+    {
+        Task<ItemResponse<SiteContentStoreCosmosDbDto>> CreateItemAsync(SiteContentStoreCosmosDbDto item);
+        Task<ItemResponse<SiteContentStoreCosmosDbDto>> DeleteItemAsync(string id, string uri);
+        Task<ItemResponse<SiteContentStoreCosmosDbDto>> GetItemAsync(string id, string uri);
+        Task<ItemResponse<SiteContentStoreCosmosDbDto>> UpdateItemAsync(string id, SiteContentStoreCosmosDbDto item);
+        Task<List<SiteContentStoreCosmosDbDto>> GetItemsAsync(string query);
+        Task<SiteContentStoreCosmosDbDto> GetByUuidAsync(string uuid);
+    }
+}
