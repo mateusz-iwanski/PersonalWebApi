80bef2c7d46d5c63ac263cd66dbe2f5e44fded4e
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-27 16:50:37 +0100

    Add UploadFileToTempRequestDto for Azure file uploads
    Added UploadFileToTempRequestDto.cs in PersonalWebApi.Models.Azure.
    Introduced UploadFileToTempRequestDto class for Azure Blob Storage.
    Properties include:
    - File (IFormFile, required)
    - TtlInDays (double, required, 0.1 to double.MaxValue, default 7)
    - Overwrite (bool, default true)
    - Metadata (Dictionary<string, string>?, optional)
    Added data annotations for validation and a summary comment.
    Note: Metadata does not work with Swagger.

---
+++ Models/Azure/UploadFileToTempRequestDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalWebApi.Models.Azure
+{
+    /// <summary>
+    /// Using for uploading file to Azure Blob Storage Account.
+    /// Metadata it's not working with swagger, it's always null
+    /// </summary>
+    public class UploadFileToTempRequestDto
+    {
+        [Required]
+        public IFormFile File { get; set; }
+
+        [Required]
+        [Range(0.1, double.MaxValue)]
+        public double TtlInDays { get; set; } = 7;
+
+        public bool Overwrite { get; set; } = true;
+
+        public Dictionary<string, string>? Metadata { get; set; }
+    }
+
+}
