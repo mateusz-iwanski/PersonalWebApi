199e9bcde6af7b1b0066440d5253d30244e6afc3
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-27 16:51:05 +0100

    Add CosmosDbBase class for Azure Cosmos DB interaction
    Introduce CosmosDbBase class in PersonalWebApi.Services.Azure
    namespace to interact with Azure Cosmos DB. Key components include:
    - Protected readonly fields: _cosmosClient and _container
    - Constructor initializing fields and ensuring DB/container creation
    - CreateDatabaseAndContainerIfNotExistsAsync method
    - GetItemsAsync method for querying Cosmos DB container
    Include necessary using directives for Microsoft.Azure.Cosmos and
    PersonalWebApi.Models.Azure.

---
+++ Services/Azure/CosmosDbBase.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Azure.Cosmos;
+using PersonalWebApi.Models.Azure;
+
+namespace PersonalWebApi.Services.Azure
+{
+    public class CosmosDbBase
+    {
+        protected readonly CosmosClient _cosmosClient;
+        protected readonly Container _container;
+
+        public CosmosDbBase(string connectionString, string databaseName, string containerName, string partitionKeyPath)
+        {
+            _cosmosClient = new CosmosClient(connectionString);
+            _container = _cosmosClient.GetContainer(databaseName, containerName);
+            CreateDatabaseAndContainerIfNotExistsAsync(databaseName, containerName, partitionKeyPath).Wait();
+        }
+
+        protected async Task CreateDatabaseAndContainerIfNotExistsAsync(string databaseName, string containerName, string partitionKeyPath)
+        {
+            // Create database if it doesn't exist
+            await _cosmosClient.CreateDatabaseIfNotExistsAsync(databaseName);
+            // Create container if it doesn't exist with partition key
+            var containerProperties = new ContainerProperties
+            {
+                Id = containerName,
+                PartitionKeyPath = $"/{partitionKeyPath}"
+            };
+            await _cosmosClient.GetDatabase(databaseName).CreateContainerIfNotExistsAsync(containerProperties);
+        }
+
+        public async Task<List<SiteContentStoreCosmosDbDto>> GetItemsAsync(string query)
+        {
+            var queryDefinition = new QueryDefinition(query);
+            var iterator = _container.GetItemQueryIterator<SiteContentStoreCosmosDbDto>(queryDefinition);
+            var results = new List<SiteContentStoreCosmosDbDto>();
+
+            while (iterator.HasMoreResults)
+            {
+                var response = await iterator.ReadNextAsync();
+                results.AddRange(response);
+            }
+
+            return results;
+        }
+
+    }
+}
