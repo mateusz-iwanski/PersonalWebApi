0a1043b7f6609e6d28af2647efa690459daa9a9b
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-30 19:35:12 +0100

    Add KernelMemory service extension method
    Introduced a new namespace `PersonalWebApi.Extensions` and a static class `KernelMemoryClientExtensions`. Added an extension method `AddKernelMemoryServices` to simplify the setup of `IKernelMemory` services using OpenAI defaults. This method builds a service provider, retrieves `SemanticKernelOptions`, creates an `IKernelMemory` instance, and registers it as a singleton service.

---
+++ Extensions/KernelMemoryClientExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.KernelMemory;
+using Microsoft.KernelMemory.DataFormats.AzureAIDocIntel;
+using PersonalWebApi.Extensions.ExtensionsSettings;
+using System.Reflection.PortableExecutable;
+
+namespace PersonalWebApi.Extensions
+{
+    /// <summary>
+    /// Extension methods for <see cref="IKernelMemory"/> and service registration.
+    /// </summary>
+    internal static class KernelMemoryClientExtensions
+    {
+        /// <summary>
+        /// Inject <see cref="IKernelMemory"/>.
+        /// </summary>
+        public static WebApplicationBuilder AddKernelMemoryServices(this WebApplicationBuilder builder)
+        {
+            var serviceProvider = builder.Services.BuildServiceProvider();
+            var semanticKernelOptions = serviceProvider.GetRequiredService<IOptions<SemanticKernelOptions>>().Value;
+
+            IKernelMemory memory = new KernelMemoryBuilder()
+                .WithOpenAIDefaults(semanticKernelOptions.Access.OpenAi.ApiKey)
+                .Build<MemoryServerless>();
+
+            builder.Services.AddSingleton(memory);
+
+            return builder;
+        }
+    }
+}
