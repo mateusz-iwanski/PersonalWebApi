f01fc95144f8a94bea96ab4eadfe90668631af5f
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-04 00:28:52 +0100

    Refactor IBlobStorageService interface methods
    Renamed `UploadToLibrary` to `UploadToLibraryAsync` and added an optional `fileId` parameter. Added a new method `DownloadFileAsync` to download files from a URI and return them as a stream. These changes enhance the functionality and flexibility of the blob storage service.

--- Services/Services.Azure/IBlobStorageService.cs
+++ Services/Services.Azure/IBlobStorageService.cs
@@ -6,12 +6,13 @@ namespace PersonalWebApi.Services.Azure
     public interface IBlobStorageService
     {
         Task<Uri> UploadToTempAsync(IFormFile file, double ttlInDays, bool overwrite = true, Dictionary<string, string>? metadata = null);
-        Task<Uri> UploadToLibrary(IFormFile file, bool overwrite = false, Dictionary<string, string>? metadata = null);
+        Task<Uri> UploadToLibraryAsync(IFormFile file, bool overwrite = false, Dictionary<string, string>? metadata = null, string fileId = "");
         Task DeleteFileFromTemp(string fileName);
         Task DeleteFileFromLibrary(string fileName);
         Task<Uri> UploadFromUriToTemp(string fileUri, string fileName, double ttlInDays, bool overwrite = false, Dictionary<string, string>? metadata = null);
         Task<Uri> UploadFromUriToLibrary(string fileUri, string fileName, bool overwrite = false, Dictionary<string, string>? metadata = null);
         Task<List<BlobItem>> GetFilesWithMetadataAsync(string containerName);
         Task<List<string>> GetContainersAsync();
+        Task<Stream> DownloadFileAsync(Uri fileUri);
     }
 }
