f75d335440dfb5e3a6fd3a2b9286d1f68ff9d0dd
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-04 00:29:05 +0100

    Add DocumentReaderDocx class to read DOCX files
    Introduce DocumentReaderDocx class in PersonalWebApi.Services.Services.DocumentReaders namespace.
    - Implements IDocumentReaderDocx interface.
    - Depends on IBlobStorageService, injected via constructor.
    - Adds ReadAsync method to download and read DOCX files.
    - Uses WordprocessingDocument from DocumentFormat.OpenXml.Wordprocessing.
    - Extracts text by iterating through document paragraphs.
    - Returns extracted text as a string.

---
+++ Services/Services.DocumentReaders/DocumentReaderDocx.cs
@@ -0,0 +1,34 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using PersonalWebApi.Services.Azure;
+using System.Text;
+
+namespace PersonalWebApi.Services.Services.DocumentReaders
+{
+    public class DocumentReaderDocx : IDocumentReaderDocx
+    {
+        private readonly IBlobStorageService _blobService;
+
+        public DocumentReaderDocx(IBlobStorageService blobService)
+        {
+            _blobService = blobService;
+        }
+
+        public async Task<string> ReadAsync(Uri fileUri)
+        {
+            StringBuilder text = new StringBuilder();
+
+            using (var stream = await _blobService.DownloadFileAsync(fileUri))
+            using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(stream, false))
+            {
+                Body body = wordDoc.MainDocumentPart.Document.Body;
+                foreach (var paragraph in body.Elements<Paragraph>())
+                {
+                    text.AppendLine(paragraph.InnerText);
+                }
+            }
+
+            return text.ToString();
+        }
+    }
+}
