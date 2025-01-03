c1708594bb1ff251c727c87393cab621a7b35c8b
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-04 00:28:16 +0100

    Comment out Qdrant config in KernelMemoryClientExtensions
    Added new using directives for `Elastic.Transport` and `PersonalWebApi.Exceptions`. Commented out code in `AddKernelMemoryServices` method that retrieves QdrantController endpoint and API key from configuration. Also commented out the method call to configure `KernelMemoryBuilder` with Qdrant memory database settings.

--- Extensions/KernelMemoryClientExtensions.cs
+++ Extensions/KernelMemoryClientExtensions.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.Options;
+﻿using Elastic.Transport;
+using Microsoft.Extensions.Options;
 using Microsoft.KernelMemory;
 using Microsoft.KernelMemory.DataFormats.AzureAIDocIntel;
+using PersonalWebApi.Exceptions;
 using PersonalWebApi.Extensions.ExtensionsSettings;
 using System.Reflection.PortableExecutable;
 
@@ -18,9 +20,16 @@ namespace PersonalWebApi.Extensions
         {
             var serviceProvider = builder.Services.BuildServiceProvider();
             var semanticKernelOptions = serviceProvider.GetRequiredService<IOptions<SemanticKernelOptions>>().Value;
+            
+            //var qdrantEndpoint = builder.Configuration.GetSection("Services:QdrantController:Endpoint").Value ??
+            //    throw new SettingsException("QdrantController Endpoint not exists in appsettings");
+            //var qdrantKey = builder.Configuration.GetSection("Services:QdrantController:APIKey").Value ??
+            //    throw new SettingsException("QdrantController APIKey not exists in appsettings");
+
 
             IKernelMemory memory = new KernelMemoryBuilder()
                 .WithOpenAIDefaults(semanticKernelOptions.Access.OpenAi.ApiKey)
+                //.WithQdrantMemoryDb(endpoint: qdrantEndpoint, apiKey: qdrantKey)
                 .Build<MemoryServerless>();
 
             builder.Services.AddSingleton(memory);
