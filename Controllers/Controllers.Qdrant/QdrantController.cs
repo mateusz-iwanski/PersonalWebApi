a6bdce0ba2fba2ce3ac9ee4710db38e6b5f70f64
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-05 23:26:05 +0100

    Refactor QdrantController and update configuration
    Updated using directives to include necessary namespaces. Refactored QdrantController to use IQdrantFileService and updated its constructor accordingly. Renamed AddToQdrant to InsertDocumentToQdrant, updated its route, and added XML documentation. Changed configuration settings retrieval to use Qdrant section and added new settings. Enhanced Setup and AddAsync methods of _qdrant with additional parameters. Updated InsertDocumentToQdrant to return fileUUID as a string.

--- Controllers/Controllers.Qdrant/QdrantController.cs
+++ Controllers/Controllers.Qdrant/QdrantController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using Elastic.Transport;
+using Microsoft.AspNetCore.Mvc;
 using PersonalWebApi.Exceptions;
 using PersonalWebApi.Services.Services.Qdrant;
-using static PersonalWebApi.Services.Services.Qdrant.QdrantCustom;
+using Qdrant.Client.Grpc;
+using System.Security.Claims;
+using static PersonalWebApi.Services.Services.Qdrant.QdrantFileService;
 
 namespace PersonalWebApi.Controllers.Controllers.Qdrant
 {
@@ -9,41 +13,82 @@ namespace PersonalWebApi.Controllers.Controllers.Qdrant
     [ApiController]
     public class QdrantController : ControllerBase
     {
-        private readonly IQdrant _qdrant;
+        private readonly IQdrantFileService _qdrant;
         private readonly IConfiguration _configuration;
 
-        public QdrantController(IQdrant qdrant, IConfiguration configuration)
+        public QdrantController(IQdrantFileService qdrant, IConfiguration configuration)
         {
             _qdrant = qdrant;
             _configuration = configuration;
         }
 
-        [HttpPost("file-add")]
-        public async Task<string> AddToQdrant(IFormFile document)
+
+
+        /// <summary>
+        /// Adds a document to the Qdrant collection.
+        /// </summary>
+        /// <param name="document">The document to be added.</param>
+        /// <param name="converationId">The ID of the conversation.</param>
+        /// <returns>File UUID as string.</returns>
+        /// <response code="200">Returns the result of the operation</response>
+        /// <response code="400">If the input is invalid</response>
+        /// <response code="500">If there is an internal server error</response>
+        /// <remarks>
+        /// First upload to Azure Blob Storage, then add to Qdrant.
+        /// Including embeddings and metadata with generated tags, summaries, and other useful information.
+        /// The collection details are retrieved from the configuration settings.
+        /// </remarks>
+        [HttpPost("{converationId:guid}/file/add")]
+        public async Task<string> InsertDocumentToQdrant(IFormFile document, Guid converationId)
         {
-            var collectionName = _configuration.GetSection("QdrantCloud:FileCollection:Name").Value ??
-                throw new SettingsException("QdrantCloud FileCollection->Name not exists in appsettings");
 
-            var collectionDistance = _configuration.GetSection("QdrantCloud:FileCollection:Distance").Value ??
-                throw new SettingsException("QdrantCloud FileCollection->Distance not exists in appsettings");
+            var collectionName = _configuration.GetSection("Qdrant:FileCollection:Name").Value ??
+               throw new SettingsException("Qdrant:FileCollection:Name not exists in appsettings");
 
-            var collectionSize = int.Parse(_configuration.GetSection("QdrantCloud:FileCollection:Size").Value ??
-                throw new SettingsException("QdrantCloud FileCollection->Size not exists in appsettings"));
+            var collectionDistance = _configuration.GetSection("Qdrant:FileCollection:Distance").Value ??
+                throw new SettingsException("Qdrant:FileCollection:Distance not exists in appsettings");
 
-            var modelEmbedding = _configuration.GetSection("QdrantCloud:FileCollection:OpenAiModelEmbedding").Value ??
-                throw new SettingsException("QdrantCloud FileCollection->OpenAiModelEmbedding not exists in appsettings");
+            var collectionSize = ulong.Parse(_configuration.GetSection("Qdrant:FileCollection:Size").Value ??
+                throw new SettingsException("Qdrant:FileCollection:Size not exists in appsettings"));
 
-            var userName = User.Identity.Name;
+            var modelEmbedding = _configuration.GetSection("Qdrant:FileCollection:OpenAiModelEmbedding").Value ??
+                throw new SettingsException("Qdrant:FileCollection:OpenAiModelEmbedding not exists in appsettings");
 
-            return await _qdrant.AddToQdrant(
-                document: document, 
-                userName: userName, 
-                collectionName: collectionName,
-                collectionDistance: collectionDistance,
-                collectionSize: collectionSize,
+            var modelApiKey = _configuration.GetSection("OpenAI:Access:ApiKey").Value ??
+                throw new SettingsException("OpenAI:Access:ApiKey not exists in appsettings");
+
+            var qdrantApiKey = _configuration.GetSection("Qdrant:Access:Key").Value ??
+                throw new SettingsException("Qdrant:Access:Key not exists in appsettings");
+
+            var qdrantUrl = _configuration.GetSection("Qdrant:Access:Uri").Value ??
+                throw new SettingsException("Qdrant:Access:Uri not exists in appsettings");
+
+            var maxTokenFileChunked = _configuration.GetSection("Qdrant:MaxTokenFileChunked").Value ??
+                throw new SettingsException("Qdrant:MaxTokenFileChunked not exists in appsettings");
+
+            var maxSummaryFileCharacters = int.Parse(_configuration.GetSection("Qdrant:MaxSummaryFileCharacters").Value ??
+                throw new SettingsException("Qdrant:MaxSummaryFileCharacters not exists in appsettings"));
+
+            _qdrant.Setup(
                 modelEmbedding: modelEmbedding,
-                overwrite: true
+                modelEmbeddingApiKey: modelApiKey,
+                qdrantUri: qdrantUrl,
+                qdrantApiKey: qdrantApiKey,
+                qdrantCollectionName: collectionName,
+                qdrantCollectionDistance: Distance.Cosine,
+                qdrantCollectionSize: collectionSize,
+                overwrite: true,
+                user: User
                 );
+
+            var fileUUID = await _qdrant.AddAsync(
+                document: document,
+                conversationUuid: converationId,
+                maxTokensPerLine: int.Parse(maxTokenFileChunked),
+                maxSummaryCharacters: maxSummaryFileCharacters
+                );
+
+            return fileUUID.ToString();
         }
     }
 }
