be30062b5c9cb84e93816d66750ae6746d4895c0
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-05 23:31:58 +0100

    Add QdrantFileSearchResultType class to Qdrant namespace
    A new class `QdrantFileSearchResultType` has been added to the
    `PersonalWebApi.Utilities.Utilities.Qdrant` namespace. This class
    contains four properties: `Id`, `Payload`, `Score`, and `Version`,
    which are used to represent the result of a file search in the
    Qdrant system.

---
+++ Utilities/Utilities.Qdrant/QdrantFileSearchResultType.cs
@@ -0,0 +1,11 @@
+﻿namespace PersonalWebApi.Utilities.Utilities.Qdrant
+{
+    public class QdrantFileSearchResultType
+    {
+        public string Id { get; set; }
+        public QdrantFilePayloadType Payload { get; set; }
+        public float Score { get; set; }
+        public string Version { get; set; }
+    }
+
+}
