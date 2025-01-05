689b2102d60b5bb2d9edbff8448faab925ff1528
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-05 23:29:56 +0100

    Add IQdrantFileService interface with Setup and AddAsync methods
    Introduced a new interface `IQdrantFileService` in the
    `PersonalWebApi.Services.Services.Qdrant` namespace. This interface
    defines two methods: `Setup` and `AddAsync`. The `Setup` method
    configures parameters related to the Qdrant service, including model
    embedding, API keys, URI, collection name, distance, collection size,
    and user claims. The `AddAsync` method is asynchronous and adds a
    document to the service, taking parameters such as the document file,
    conversation UUID, maximum tokens per line, and maximum summary
    characters.

---
+++ Services/Services.Qdrant/IQdrantFileService.cs
@@ -0,0 +1,23 @@
+﻿using Qdrant.Client.Grpc;
+using System.Security.Claims;
+using static PersonalWebApi.Services.Services.Qdrant.QdrantFileService;
+
+namespace PersonalWebApi.Services.Services.Qdrant
+{
+    public interface IQdrantFileService
+    {
+        void Setup(
+            string modelEmbedding,
+            string modelEmbeddingApiKey,
+            string qdrantUri,
+            string qdrantApiKey,
+            string qdrantCollectionName,
+            Distance qdrantCollectionDistance,
+            ulong qdrantCollectionSize,
+            bool overwrite,
+            ClaimsPrincipal user
+            );
+
+        Task<Guid> AddAsync(IFormFile document, Guid conversationUuid, int maxTokensPerLine, int maxSummaryCharacters);
+    }
+}
