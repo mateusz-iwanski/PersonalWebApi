72751dac06a7866fb6e032f28de7ae69bd3f79da
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-06 23:26:52 +0100

    Add CosmosDbDtoBase class with attributes and abstract methods
    Introduced a new namespace `PersonalWebApi.Models.Models.Azure` and
    created an abstract class `CosmosDbDtoBase`. Added `Required` and
    `JsonProperty` attributes to the `Id` and `ConversationUuid` properties.
    Declared two abstract methods: `ContainerName` and `PartitionKey`.
    Included new using directives for `Newtonsoft.Json`,
    `System.ComponentModel`, and `System.ComponentModel.DataAnnotations`.

---
+++ Models/Models.Azure/CosmosDbDtoBase.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalWebApi.Models.Models.Azure
+{
+    public abstract class CosmosDbDtoBase
+    {
+        [Required]
+        [JsonProperty(PropertyName = "id")]
+        public Guid Id { get; } = Guid.NewGuid(); // Unique identifier. It can be GUID or uuid session from ai agent
+
+        [Required]
+        [JsonProperty(PropertyName = "conversationUuid")]
+        public string ConversationUuid { get; set; } = string.Empty;
+
+        abstract public string ContainerName();
+        abstract public string PartitionKey(); 
+
+    }
+}
