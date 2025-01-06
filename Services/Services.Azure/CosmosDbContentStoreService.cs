3f04401c6cd448f42a5662077e7b6d15c91fb84d
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-06 23:28:06 +0100

    Refactor CosmosDbContentStoreService for flexibility
    Refactored CosmosDbContentStoreService to introduce generic methods and enhance flexibility and reusability. Updated constructor to use configuration sections for connection strings and database names. Generalized CreateItemAsync, GetItemAsync, UpdateItemAsync, and DeleteItemAsync methods to accept any type inheriting from CosmosDbDtoBase. Added GetByQueryAsync for flexible querying. Removed specific methods for SiteContentStoreCosmosDbDto. Added necessary using directives and XML documentation.

--- Services/Services.Azure/CosmosDbContentStoreService.cs
+++ Services/Services.Azure/CosmosDbContentStoreService.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.Azure.Cosmos;
+using MongoDB.Driver.Core.Configuration;
+using Newtonsoft.Json;
 using PersonalWebApi.Models;
 using PersonalWebApi.Models.Azure;
+using PersonalWebApi.Models.Models.Azure;
+using PersonalWebApi.Services.Services.History;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 
 namespace PersonalWebApi.Services.Azure
@@ -10,43 +15,49 @@ namespace PersonalWebApi.Services.Azure
     {
         // TODO: add settings to appsettings.AzureService.json
         public CosmosDbContentStoreService(IConfiguration configuration)
-            : base(configuration.GetConnectionString("PersonalApiDbCosmos"), "PersonalApi", "Items", "uri")
+            : base(
+                  configuration.GetSection("Azure:CosmosDb:Connection").Value,
+                  configuration.GetSection("Azure:CosmosDb:CosmosDbDatabaseName").Value
+                  )
         {
         }
 
-        public async Task<ItemResponse<SiteContentStoreCosmosDbDto>> CreateItemAsync(SiteContentStoreCosmosDbDto item)
+        public async Task<ItemResponse<T>> CreateItemAsync<T>(T cosmosDto) where T : CosmosDbDtoBase
         {
-            if (item == null || string.IsNullOrEmpty(item.Uri))
+            await CreateDatabaseAndContainerIfNotExistsAsync(_databaseName, cosmosDto.ContainerName(), cosmosDto.PartitionKey());
+            var container = _cosmosClient.GetContainer(_databaseName, cosmosDto.ContainerName());
+            return await container.CreateItemAsync(cosmosDto, new PartitionKey(cosmosDto.PartitionKey()));
+        }
+
+        public async Task<ItemResponse<T>> GetItemAsync<T>(string id, string containerName, string uri) where T : CosmosDbDtoBase
+        {
+            if (string.IsNullOrEmpty(id) || string.IsNullOrEmpty(uri) || string.IsNullOrEmpty(containerName))
             {
-                throw new ArgumentException("Item or partition key (Uri) cannot be null or empty.");
+                throw new ArgumentException("Id, partition key or container name cannot be null or empty.");
             }
-            return await _container.CreateItemAsync(item, new PartitionKey(item.Uri));
+            var container = _cosmosClient.GetContainer(_databaseName, containerName);
+            return await container.ReadItemAsync<T>(id, new PartitionKey(uri));
         }
 
-        public async Task<ItemResponse<object>> CreateItemAsync(object item)
+        /// <summary>
+        /// Get items by query from a container by query
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="query"></param>
+        /// <param name="containerName"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// 
+        /// Example query:
+        /// 
+        ///     var query = $"SELECT * FROM c WHERE c.uuid = @uuid";
+        ///     QueryDefinition queryDefinition = new QueryDefinition(query).WithParameter("@uuid", uuid);
+        /// 
+        /// </remarks>
+        public async Task<T?> GetByQueryAsync<T>(QueryDefinition query, string containerName) where T : CosmosDbDtoBase
         {
-            if (item == null)
-            {
-                throw new ArgumentException("Item or partition key (Uri) cannot be null or empty.");
-            }
-            return await _container.CreateItemAsync(item);
-        }
-
-        public async Task<ItemResponse<SiteContentStoreCosmosDbDto>> GetItemAsync(string id, string uri)
-        {
-            if (string.IsNullOrEmpty(id) || string.IsNullOrEmpty(uri))
-            {
-                throw new ArgumentException("Id and partition key (Uri) cannot be null or empty.");
-            }
-            return await _container.ReadItemAsync<SiteContentStoreCosmosDbDto>(id, new PartitionKey(uri));
-        }
-
-        public async Task<SiteContentStoreCosmosDbDto> GetByUuidAsync(string uuid)
-        {
-            var query = $"SELECT * FROM c WHERE c.uuid = @uuid";
-            var queryDefinition = new QueryDefinition(query).WithParameter("@uuid", uuid);
-
-            var queryResultSetIterator = _container.GetItemQueryIterator<SiteContentStoreCosmosDbDto>(queryDefinition);
+            var container = _cosmosClient.GetContainer(_databaseName, containerName);
+            var queryResultSetIterator = container.GetItemQueryIterator<T>(query);
 
             if (queryResultSetIterator.HasMoreResults)
             {
@@ -54,34 +65,34 @@ namespace PersonalWebApi.Services.Azure
                 return response.FirstOrDefault(); // Return the first matching document or null if none found
             }
 
-            return null; // No results found
+            return default(T); // No results found
         }
 
-        public async Task<ItemResponse<SiteContentStoreCosmosDbDto>> GetItemAsync(string uuid)
+        /// <summary>
+        /// Updates an item in the Cosmos DB container.
+        /// </summary>
+        /// <param name="id">The ID of the item to update.</param>
+        /// <param name="item">The item to update.</param>
+        /// <returns>The updated item response.</returns>
+        /// <exception cref="ArgumentException">Thrown when the item or partition key (uri) is null or empty.</exception>
+        public async Task<ItemResponse<T>> UpdateItemAsync<T>(T item) where T : CosmosDbDtoBase
         {
-            if (string.IsNullOrEmpty(uuid))
-            {
-                throw new ArgumentException("Id cannot be null or empty.");
-            }
-            return await _container.ReadItemAsync<SiteContentStoreCosmosDbDto>(uuid, new PartitionKey(uuid));
+            var container = _cosmosClient.GetContainer(_databaseName,  item.ContainerName());
+            return await container.ReplaceItemAsync(item, item.Id.ToString(), new PartitionKey(item.PartitionKey()));
         }
 
-        public async Task<ItemResponse<SiteContentStoreCosmosDbDto>> UpdateItemAsync(string id, SiteContentStoreCosmosDbDto item)
+        /// <summary>
+        /// Updates an item in the Cosmos DB container.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="uri"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public async Task<ItemResponse<T>> DeleteItemAsync<T>(T item) where T : CosmosDbDtoBase
         {
-            if (item == null || string.IsNullOrEmpty(item.Uri))
-            {
-                throw new ArgumentException("Item or partition key (Uri) cannot be null or empty.");
-            }
-            return await _container.ReplaceItemAsync(item, id, new PartitionKey(item.Uri));
+            var container = _cosmosClient.GetContainer(_databaseName, item.ContainerName());
+            return await container.DeleteItemAsync<T>(item.Id.ToString(), new PartitionKey(item.PartitionKey()));
         }
 
-        public async Task<ItemResponse<SiteContentStoreCosmosDbDto>> DeleteItemAsync(string id, string uri)
-        {
-            if (string.IsNullOrEmpty(id) || string.IsNullOrEmpty(uri))
-            {
-                throw new ArgumentException("Id and partition key (Uri) cannot be null or empty.");
-            }
-            return await _container.DeleteItemAsync<SiteContentStoreCosmosDbDto>(id, new PartitionKey(uri));
-        }
     }
 }
