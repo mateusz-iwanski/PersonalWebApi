0f7ec19ea44bc706d1d00a6e0a82279b37730c4f
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-06 23:27:50 +0100

    Refactor CosmosDbBase and add new listing methods
    Updated `using` directives to include `PersonalWebApi.Models.Models.Azure` and `System.Net`. Replaced `_container` field with `_databaseName`. Modified `CosmosDbBase` constructor to accept only `connectionString` and `databaseName` parameters. Changed `CreateDatabaseAndContainerIfNotExistsAsync` method from `protected` to `public` and added a delay for database readiness. Updated `GetItemsAsync` to be generic and require a `containerName` parameter. Added `ListAllDatabasesAsync` and `ListAllContainersAsync` methods.

--- Services/Services.Azure/CosmosDbBase.cs
+++ Services/Services.Azure/CosmosDbBase.cs
@@ -1,24 +1,33 @@
 ﻿using Microsoft.Azure.Cosmos;
 using PersonalWebApi.Models.Azure;
+using PersonalWebApi.Models.Models.Azure;
+using System.Net;
 
 namespace PersonalWebApi.Services.Azure
 {
     public class CosmosDbBase
     {
         protected readonly CosmosClient _cosmosClient;
-        protected readonly Container _container;
+        protected readonly string _databaseName;
+        //protected readonly Container _container;
 
-        public CosmosDbBase(string connectionString, string databaseName, string containerName, string partitionKeyPath)
+        public CosmosDbBase(string connectionString, string databaseName)
         {
             _cosmosClient = new CosmosClient(connectionString);
-            _container = _cosmosClient.GetContainer(databaseName, containerName);
-            CreateDatabaseAndContainerIfNotExistsAsync(databaseName, containerName, partitionKeyPath).Wait();
+            _databaseName = databaseName;
+            
         }
 
-        protected async Task CreateDatabaseAndContainerIfNotExistsAsync(string databaseName, string containerName, string partitionKeyPath)
+        public async Task CreateDatabaseAndContainerIfNotExistsAsync(string databaseName, string containerName, string partitionKeyPath)
         {
             // Create database if it doesn't exist
-            await _cosmosClient.CreateDatabaseIfNotExistsAsync(databaseName);
+            var databaseResponse = await _cosmosClient.CreateDatabaseIfNotExistsAsync(databaseName);
+
+            // Ensure at least 1 second delay if the database was created
+            // Sometimes an error appears: the database is not ready and we want to insert a new container into it
+            if (databaseResponse.StatusCode == HttpStatusCode.Created)
+                await Task.Delay(1000); 
+
             // Create container if it doesn't exist with partition key
             var containerProperties = new ContainerProperties
             {
@@ -28,11 +37,14 @@ namespace PersonalWebApi.Services.Azure
             await _cosmosClient.GetDatabase(databaseName).CreateContainerIfNotExistsAsync(containerProperties);
         }
 
-        public async Task<List<SiteContentStoreCosmosDbDto>> GetItemsAsync(string query)
+        public async Task<List<T>> GetItemsAsync<T>(string containerName, string query) where T : CosmosDbDtoBase
         {
             var queryDefinition = new QueryDefinition(query);
-            var iterator = _container.GetItemQueryIterator<SiteContentStoreCosmosDbDto>(queryDefinition);
-            var results = new List<SiteContentStoreCosmosDbDto>();
+
+            var container = _cosmosClient.GetContainer(_databaseName, containerName);
+            var iterator = container.GetItemQueryIterator<T>(queryDefinition);
+
+            var results = new List<T>();
 
             while (iterator.HasMoreResults)
             {
@@ -42,6 +54,41 @@ namespace PersonalWebApi.Services.Azure
 
             return results;
         }
+
+        public async Task<List<string>> ListAllDatabasesAsync()
+        {
+            var databases = new List<string>();
+            using (var iterator = _cosmosClient.GetDatabaseQueryIterator<DatabaseProperties>())
+            {
+                while (iterator.HasMoreResults)
+                {
+                    var response = await iterator.ReadNextAsync();
+                    databases.AddRange(response.Select(db => db.Id));
+                }
+            }
+            return databases;
+        }
+
+        public async Task<List<string>> ListAllContainersAsync(string databaseName)
+        {
+            if (string.IsNullOrEmpty(databaseName))
+            {
+                throw new ArgumentException("Database name cannot be null or empty.");
+            }
+
+            var containers = new List<string>();
+            var database = _cosmosClient.GetDatabase(databaseName);
+            using (var iterator = database.GetContainerQueryIterator<ContainerProperties>())
+            {
+                while (iterator.HasMoreResults)
+                {
+                    var response = await iterator.ReadNextAsync();
+                    containers.AddRange(response.Select(container => container.Id));
+                }
+            }
+            return containers;
+        }
+
 
     }
 }
