a6a086e0740054c99a35b975ee5ec14e0af10bf3
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-07 18:12:00 +0100

    Add ICosmosDbService interface for Cosmos DB operations
    Introduce ICosmosDbService in PersonalWebApi.Services.Azure.
    This interface defines async CRUD methods for Cosmos DB, using
    generics constrained to CosmosDbDtoBase. Added necessary using
    directives for Microsoft.Azure.Cosmos and relevant models.

---
+++ Services/Services.Azure/ICosmosDbService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Cosmos;
+using PersonalWebApi.Models.Azure;
+using PersonalWebApi.Models.Models.Azure;
+
+namespace PersonalWebApi.Services.Azure
+{
+    public interface ICosmosDbService
+    {
+        Task<ItemResponse<T>> CreateItemAsync<T>(T cosmosDto) where T : CosmosDbDtoBase;
+        Task<ItemResponse<T>> GetItemAsync<T>(string id, string containerName, string uri) where T : CosmosDbDtoBase;
+        Task<T?> GetByQueryAsync<T>(QueryDefinition query, string containerName) where T : CosmosDbDtoBase;
+        Task<ItemResponse<T>> UpdateItemAsync<T>(T item) where T : CosmosDbDtoBase;
+        Task<ItemResponse<T>> DeleteItemAsync<T>(string itemId, string containerName, string partitionKeyData) where T : CosmosDbDtoBase;
+    }
+}
