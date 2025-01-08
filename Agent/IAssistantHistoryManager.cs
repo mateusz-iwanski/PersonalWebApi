9a817658807ba24d18d1117257f7e34bc452850b
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-08 23:37:30 +0100

    Add IAssistantHistoryManager interface for chat history
    Introduced a new namespace `PersonalWebApi.Agent` and defined the `IAssistantHistoryManager` interface within it. This interface includes two methods: `LoadAsync` for loading chat history and `SaveAsync` for saving chat history. Added necessary using directives for `Microsoft.KernelMemory` and `PersonalWebApi.Models.Models.Azure`.

---
+++ Agent/IAssistantHistoryManager.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.KernelMemory;
+using PersonalWebApi.Models.Models.Azure;
+
+namespace PersonalWebApi.Agent
+{
+    public interface IAssistantHistoryManager
+    {
+        Task LoadAsync(Guid conversationUuid, IKernelMemory memory);
+        Task<ChatHistoryStoreDbDto> SaveAsync(Guid sessionUuid, Guid conversationUuid, string message);
+    }
+}
