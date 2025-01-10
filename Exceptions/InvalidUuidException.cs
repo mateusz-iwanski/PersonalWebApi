8a393cb891e066cc4e640c3d91526b9c1d50730b
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-10 20:11:48 +0100

    Add InvalidUuidException to handle invalid UUID errors
    A new namespace `PersonalWebApi.Exceptions` has been added.
    A new class `InvalidUuidException` has been introduced, which inherits from `CustomException`.
    The `InvalidUuidException` class includes two constructors:
    - One that takes a `string` message and passes it to the base class constructor.
    - Another that takes a `string` message and an `Exception` inner exception, and passes both to the base class constructor.

---
+++ Exceptions/InvalidUuidException.cs
@@ -0,0 +1,13 @@
+﻿namespace PersonalWebApi.Exceptions
+{
+    public class InvalidUuidException : CustomException
+    {
+        public InvalidUuidException(string message) : base(message)
+        {
+        }
+
+        public InvalidUuidException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
