f912600d26f8ffeb00f37856252826eec6c9a074
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-11 16:15:29 +0100

    Add KqlApplicationInsightsApi class for KQL queries
    Introduce KqlApplicationInsightsApi class in PersonalWebApi.Utilities.Kql namespace to interact with Azure Application Insights using KQL. Add necessary using directives. Initialize Application Insights client and application ID in the constructor, throwing SettingsException if configuration is missing. Implement ExecuteQueryAsync method to execute KQL queries asynchronously.

---
+++ Utilities/Kql/KqlApplicationInsightsApi.cs
@@ -0,0 +1,31 @@
+﻿using Elastic.Clients.Elasticsearch.QueryDsl;
+using Microsoft.Azure.ApplicationInsights.Query;
+using Microsoft.Azure.ApplicationInsights.Query.Models;
+using Microsoft.Rest;
+using PersonalWebApi.Exceptions;
+
+namespace PersonalWebApi.Utilities.Kql
+{
+    public class KqlApplicationInsightsApi
+    {
+        private readonly ApplicationInsightsDataClient _client;
+        private readonly string _applicationId;
+
+        public KqlApplicationInsightsApi(IConfiguration configuration)
+        {
+            _applicationId = configuration.GetSection("Telemetry:ApplicationInsights:ApplicationId").Value ??
+                throw new SettingsException("Telemetry:ApplicationInsights:ApplicationId not exists in settings");
+
+            var apiKey = configuration.GetSection("Telemetry:ApplicationInsights:KqlClientApiKey").Value ??
+                throw new SettingsException("Telemetry:ApplicationInsights:KqlClientApiKey not exists in settings");
+
+            var credentials = new ApiKeyClientCredentials(apiKey);
+            _client = new ApplicationInsightsDataClient(credentials);
+        }
+
+        public async Task<HttpOperationResponse<QueryResults>> ExecuteQueryAsync(string query)
+        {
+            return await _client.Query.ExecuteWithHttpMessagesAsync(_applicationId, query);
+        }
+    }
+}
