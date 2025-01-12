03d48c43efdb23dc38fe86ce3c060f18a384869a
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-12 20:40:44 +0100

    Add IAssistantHistoryManager interface with async methods
    Introduce IAssistantHistoryManager interface in PersonalWebApi.Services.Services.History namespace. This interface includes LoadAsync and SaveAsync methods for handling items of type T, a subclass of CosmosDbDtoBase. Added using directives for Microsoft.KernelMemory and PersonalWebApi.Models.Models.Azure.

---
+++ Services/Services.History/IAssistantHistoryManager.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.KernelMemory;
+using PersonalWebApi.Models.Models.Azure;
+
+namespace PersonalWebApi.Services.Services.History
+{
+    public interface IAssistantHistoryManager
+    {
+        Task<List<T>> LoadAsync<T>(Guid conversationUuid) where T : CosmosDbDtoBase;
+        Task<T> SaveAsync<T>(T historyDto) where T : CosmosDbDtoBase;
+    }
+}
