654d7899f9475fe96af2e144666a1cd98f539d7c
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-15 08:49:21 +0100

    Enhance AzureBlobStorageService with logging and metadata
    Updated AzureBlobStorageService.cs to include new dependencies,
    enhanced logging, and improved metadata handling. Renamed
    methods for clarity and updated XML documentation. Enhanced
    GetFilesWithMetadataAsync to include additional blob properties
    and updated GetContainersAsync to return detailed container
    information.

--- Services/Azure/AzureBlobStorageService.cs
+++ Services/Azure/AzureBlobStorageService.cs
@@ -2,8 +2,13 @@
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using Azure.Storage.Blobs.Specialized;
+using Microsoft.AspNetCore.Http;
 using Microsoft.WindowsAzure.Storage.Shared.Protocol;
 using PersonalWebApi.Exceptions;
+using PersonalWebApi.Models.Storage;
+using PersonalWebApi.Services.Services.History;
+using PersonalWebApi.Services.Services.System;
+using SharpCompress.Common;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -17,6 +22,8 @@ namespace PersonalWebApi.Services.Azure
     public class AzureBlobStorageService : IBlobStorageService
     {
         private readonly IConfiguration _configuration;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IAssistantHistoryManager _assistantHistoryManager;
         private readonly BlobServiceClient _blobServiceClient;
         private readonly string _tempContainerName;
         private readonly string _libraryContainerName;
@@ -27,9 +34,14 @@ namespace PersonalWebApi.Services.Azure
         /// </summary>
         /// <param name="configuration">The configuration settings.</param>
         /// <exception cref="SettingsException">Thrown when required settings are missing.</exception>
-        public AzureBlobStorageService(IConfiguration configuration)
+        public AzureBlobStorageService(
+            IConfiguration configuration, 
+            IHttpContextAccessor httpContextAccessor,
+            IAssistantHistoryManager assistantHistoryManager)
         {
             _configuration = configuration;
+            _httpContextAccessor = httpContextAccessor;
+            _assistantHistoryManager = assistantHistoryManager;
 
             // TODO: remove settings to appsettings.AzureService.json
 
@@ -51,15 +63,17 @@ namespace PersonalWebApi.Services.Azure
         /// <summary>
         /// Uploads a file to the specified container in Azure Blob Storage.
         /// </summary>
-        /// <param name="file">The file to upload.</param>
+        /// <param name="file">The file to uploadAsync.</param>
         /// <param name="ttlInDays">The time-to-live in days for the file.</param>
         /// <param name="overwrite">Whether to Overwrite the file if it already exists.</param>
-        /// <param name="containerName">The name of the container to upload to.</param>
+        /// <param name="containerName">The name of the container to uploadAsync to.</param>
         /// <returns>The URI of the uploaded file.</returns>
         /// <exception cref="AzureBlobStorageException">Thrown when the blob already exists and Overwrite is set to false.</exception>
-        /// <exception cref="RequestFailedException">Thrown when an error occurs during the upload.</exception>
-        private async Task<Uri> upload(IFormFile file, double? ttlInDays, bool overwrite, string containerName, Dictionary<string, string>? metadata = null)
+        /// <exception cref="RequestFailedException">Thrown when an error occurs during the uploadAsync.</exception>
+        private async Task<Uri> uploadAsync(IFormFile file, double? ttlInDays, bool overwrite, string containerName, Dictionary<string, string>? metadata = null)
         {
+            (Guid conversationUuid, Guid sessionUuid) = ContextAccessorReader.RetrieveCrucialUuid(_httpContextAccessor);
+
             _blobContainerClient = await getContainerPublicAccessAsync(containerName, PublicAccessType.Blob);
 
             BlobClient blobClient = _blobContainerClient.GetBlobClient(file.FileName);
@@ -100,6 +114,19 @@ namespace PersonalWebApi.Services.Azure
 
             var fileUrl = await GetFileUrlAsync(file.FileName, containerName);
 
+            // ###### Log it
+            var storageEvent = new StorageEventsDto(conversationUuid, sessionUuid)
+            {
+                EventName = "upload",
+                ServiceName = "AzureBlobStorage",
+                IsSuccess = true,
+                ActionType = "Upload",
+                FileUri = fileUrl,
+                ErrorMessage = string.Empty,
+            };
+
+            await _assistantHistoryManager.SaveAsync(storageEvent);
+
             return new Uri(fileUrl);
         }
 
@@ -138,7 +165,7 @@ namespace PersonalWebApi.Services.Azure
         /// <summary>
         /// Uploads a file to the temporary container in Azure Blob Storage.
         /// </summary>
-        /// <param name="file">The file to upload.</param>
+        /// <param name="file">The file to uploadAsync.</param>
         /// <param name="ttlInDays">The time-to-live in days for the file.</param>
         /// <param name="overwrite">Whether to Overwrite the file if it already exists.</param>
         /// <param name="metadata">The metadata to add to the file.</param>
@@ -146,14 +173,14 @@ namespace PersonalWebApi.Services.Azure
         /// <remarks>Once you set ttl in the metadat, the other service will delete the file after the set time.</remarks>
         public async Task<Uri> UploadToTempAsync(IFormFile file, double ttlInDays, bool overwrite = true, Dictionary<string, string>? metadata = null)
         {
-            var uri = await upload(file, ttlInDays, overwrite, _tempContainerName, metadata);
+            var uri = await uploadAsync(file, ttlInDays, overwrite, _tempContainerName, metadata);
             return uri;
         }
 
         /// <summary>
         /// Uploads a file to the library container in Azure Blob Storage.
         /// </summary>
-        /// <param name="file">The file to upload.</param>
+        /// <param name="file">The file to uploadAsync.</param>
         /// <param name="overwrite">Whether to Overwrite the file if it already exists.</param>
         /// <returns>The URI of the uploaded file.</returns>
         /// <param name="metadata">The metadata to add to the file.</param>
@@ -165,7 +192,7 @@ namespace PersonalWebApi.Services.Azure
                 else
                     metadata = new Dictionary<string, string> { { "fileId", fileId } };
 
-            var uri = await upload(file, null, overwrite, _libraryContainerName, metadata);
+            var uri = await uploadAsync(file, null, overwrite, _libraryContainerName, metadata);
             return uri;
         }
 
@@ -194,7 +221,7 @@ namespace PersonalWebApi.Services.Azure
         /// <summary>
         /// Uploads a file from a URI to the temporary container in Azure Blob Storage.
         /// </summary>
-        /// <param name="fileUri">The URI of the file to upload.</param>
+        /// <param name="fileUri">The URI of the file to uploadAsync.</param>
         /// <param name="fileName">The name of the file.</param>
         /// <param name="ttlInDays">The time-to-live in days for the file.</param>
         /// <param name="overwrite">Whether to Overwrite the file if it already exists.</param>
@@ -208,7 +235,7 @@ namespace PersonalWebApi.Services.Azure
         /// <summary>
         /// Uploads a file from a URI to the library container in Azure Blob Storage.
         /// </summary>
-        /// <param name="fileUri">The URI of the file to upload.</param>
+        /// <param name="fileUri">The URI of the file to uploadAsync.</param>
         /// <param name="fileName">The name of the file.</param>
         /// <param name="overwrite">Whether to Overwrite the file if it already exists.</param>
         /// <param name="metadata">The metadata to add to the file.</param>
@@ -238,15 +265,17 @@ namespace PersonalWebApi.Services.Azure
         /// <summary>
         /// Uploads a file from a URI to the specified container in Azure Blob Storage.
         /// </summary>
-        /// <param name="fileUri">The URI of the file to upload.</param>
+        /// <param name="fileUri">The URI of the file to uploadAsync.</param>
         /// <param name="fileName">The name of the file.</param>
-        /// <param name="containerName">The name of the container to upload to.</param>
+        /// <param name="containerName">The name of the container to uploadAsync to.</param>
         /// <param name="ttlInDays">The time-to-live in days for the file.</param>
         /// <param name="overwrite">Whether to Overwrite the file if it already exists.</param>
         /// <param name="metadata">The metadata to add to the file.</param>
         /// <returns>The URI of the uploaded file.</returns>
         private async Task<Uri> uploadFromUriAsync(string fileUri, string fileName, string containerName, double? ttlInDays, bool overwrite = false, Dictionary<string, string>? metadata = null)
         {
+            (Guid conversationUuid, Guid sessionUuid) = ContextAccessorReader.RetrieveCrucialUuid(_httpContextAccessor);
+
             string tempFilePath = Path.GetTempFileName();
             Uri uri = null;
 
@@ -274,7 +303,7 @@ namespace PersonalWebApi.Services.Azure
                     ContentType = "application/octet-stream" // Set the appropriate content type if known
                 };
 
-                uri = await upload(file, ttlInDays, overwrite, containerName, metadata);
+                uri = await uploadAsync(file, ttlInDays, overwrite, containerName, metadata);
 
                 // Ensure fileStream is disposed before deleting the file
             }
@@ -282,10 +311,24 @@ namespace PersonalWebApi.Services.Azure
             // Delete the local file
             File.Delete(tempFilePath);
 
+            // ###### Log it
+            var storageEvent = new StorageEventsDto(conversationUuid, sessionUuid)
+            {
+                EventName = "upload",
+                ServiceName = "AzureBlobStorage",
+                IsSuccess = true,
+                ActionType = "Upload",
+                FileUri = uri.ToString(),
+                ErrorMessage = string.Empty,
+            };
+
+            await _assistantHistoryManager.SaveAsync(storageEvent);
+
             return uri;
         }
 
-        // get list of files with metadat
+        // get list of files with metadata
+        // add all properties from properties.Value to metadata
         public async Task<List<BlobItem>> GetFilesWithMetadataAsync(string containerName)
         {
             _blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
@@ -300,18 +343,66 @@ namespace PersonalWebApi.Services.Azure
                     blobItem.Metadata.Add(metadataItem);
                 }
 
+                // Add blob properties to metadata
+                blobItem.Metadata.Add("Url", blobClient.Uri.ToString());
+                blobItem.Metadata.Add("AcceptRanges", properties.Value.AcceptRanges);
+                blobItem.Metadata.Add("AccessTier", properties.Value.AccessTier);
+                blobItem.Metadata.Add("AccessTierChangedOn", properties.Value.AccessTierChangedOn.ToString());
+                blobItem.Metadata.Add("AccessTierInferred", properties.Value.AccessTierInferred.ToString());
+                blobItem.Metadata.Add("ArchiveStatus", properties.Value.ArchiveStatus);
+                blobItem.Metadata.Add("BlobCommittedBlockCount", properties.Value.BlobCommittedBlockCount.ToString());
+                blobItem.Metadata.Add("BlobCopyStatus", properties.Value.BlobCopyStatus.ToString());
+                blobItem.Metadata.Add("BlobSequenceNumber", properties.Value.BlobSequenceNumber.ToString());
+                blobItem.Metadata.Add("BlobType", properties.Value.BlobType.ToString());
+                blobItem.Metadata.Add("CacheControl", properties.Value.CacheControl);
+                blobItem.Metadata.Add("ContentDisposition", properties.Value.ContentDisposition);
+                blobItem.Metadata.Add("ContentEncoding", properties.Value.ContentEncoding);
+                blobItem.Metadata.Add("ContentHash", Convert.ToBase64String(properties.Value.ContentHash));
+                blobItem.Metadata.Add("ContentLanguage", properties.Value.ContentLanguage);
+                blobItem.Metadata.Add("ContentLength", properties.Value.ContentLength.ToString());
+                blobItem.Metadata.Add("ContentType", properties.Value.ContentType);
+                blobItem.Metadata.Add("CopyCompletedOn", properties.Value.CopyCompletedOn.ToString());
+                blobItem.Metadata.Add("CopyId", properties.Value.CopyId);
+                blobItem.Metadata.Add("CopyProgress", properties.Value.CopyProgress);
+                blobItem.Metadata.Add("CopySource", properties.Value.CopySource?.ToString());
+                blobItem.Metadata.Add("CopyStatus", properties.Value.CopyStatus.ToString());
+                blobItem.Metadata.Add("CopyStatusDescription", properties.Value.CopyStatusDescription);
+                blobItem.Metadata.Add("CreatedOn", properties.Value.CreatedOn.ToString());
+                blobItem.Metadata.Add("DestinationSnapshot", properties.Value.DestinationSnapshot);
+                blobItem.Metadata.Add("ETag", properties.Value.ETag.ToString());
+                blobItem.Metadata.Add("EncryptionKeySha256", properties.Value.EncryptionKeySha256);
+                blobItem.Metadata.Add("EncryptionScope", properties.Value.EncryptionScope);
+                blobItem.Metadata.Add("ExpiresOn", properties.Value.ExpiresOn.ToString());
+                blobItem.Metadata.Add("HasLegalHold", properties.Value.HasLegalHold.ToString());
+                blobItem.Metadata.Add("ImmutabilityPolicy", properties.Value.ImmutabilityPolicy?.ToString());
+                blobItem.Metadata.Add("IsIncrementalCopy", properties.Value.IsIncrementalCopy.ToString());
+                blobItem.Metadata.Add("IsLatestVersion", properties.Value.IsLatestVersion.ToString());
+                blobItem.Metadata.Add("IsSealed", properties.Value.IsSealed.ToString());
+                blobItem.Metadata.Add("IsServerEncrypted", properties.Value.IsServerEncrypted.ToString());
+                blobItem.Metadata.Add("LastAccessed", properties.Value.LastAccessed.ToString());
+                blobItem.Metadata.Add("LastModified", properties.Value.LastModified.ToString());
+                blobItem.Metadata.Add("LeaseDuration", properties.Value.LeaseDuration.ToString());
+                blobItem.Metadata.Add("LeaseState", properties.Value.LeaseState.ToString());
+                blobItem.Metadata.Add("LeaseStatus", properties.Value.LeaseStatus.ToString());
+                blobItem.Metadata.Add("ObjectReplicationDestinationPolicyId", properties.Value.ObjectReplicationDestinationPolicyId);
+                blobItem.Metadata.Add("ObjectReplicationSourceProperties", properties.Value.ObjectReplicationSourceProperties?.ToString());
+                blobItem.Metadata.Add("RehydratePriority", properties.Value.RehydratePriority);
+                blobItem.Metadata.Add("TagCount", properties.Value.TagCount.ToString());
+                blobItem.Metadata.Add("VersionId", properties.Value.VersionId);
+
+
                 blobs.Add(blobItem);
             }
             return blobs;
         }
 
         // get list of containers
-        public async Task<List<string>> GetContainersAsync()
+        public async Task<List<BlobContainerItem>> GetContainersAsync()
         {
-            var containers = new List<string>();
+            var containers = new List<BlobContainerItem>();
             await foreach (var container in _blobServiceClient.GetBlobContainersAsync())
             {
-                containers.Add(container.Name);
+                containers.Add(container);
             }
             return containers;
         }
