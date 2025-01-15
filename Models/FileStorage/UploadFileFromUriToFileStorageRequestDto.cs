dee72c7bf9c16621f0ccd0d47df13a599c0bdde9
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-15 11:58:11 +0100

    Add UploadFileFromUriToFileStorageRequestDto class
    A new class `UploadFileFromUriToFileStorageRequestDto` has been added to the `PersonalWebApi.Models.Storage` namespace. This class is designed for uploading a file from a URL to an Azure Blob Storage Account. It includes the following properties:
    
    - `FileUri` (string): The URI of the file to be uploaded. This property is required.
    - `FileName` (string): The name of the file to be uploaded. This property is required.
    - `Overwrite` (bool): A flag indicating whether to overwrite the file if it already exists. The default value is `true`.
    - `Metadata` (Dictionary<string, string>?): Optional metadata for the file. Note that metadata does not work with Swagger and is always null.
    
    The `System.ComponentModel.DataAnnotations` namespace is used to apply the `[Required]` attribute to the `FileUri` and `FileName` properties.

---
+++ Models/FileStorage/UploadFileFromUriToFileStorageRequestDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalWebApi.Models.Storage
+{
+    /// <summary>
+    /// Using for uploading file from Url to Azure Blob Storage Account.
+    /// Metadata it's not working with swagger, it's always null.
+    /// </summary>
+    public class UploadFileFromUriToFileStorageRequestDto
+    {
+        [Required]
+        public string FileUri { get; set; } = string.Empty;
+
+        [Required]
+        public string FileName { get; set; } = string.Empty;
+
+        public bool Overwrite { get; set; } = true;
+
+        public Dictionary<string, string>? Metadata { get; set; }
+    }
+}
