2cce74f6616f81cd3308f8534938e4be0d6cf7d4
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-15 08:49:33 +0100

    Update GetContainersAsync to return List<BlobContainerItem>
    Changed the return type of GetContainersAsync in IBlobStorageService
    from Task<List<string>> to Task<List<BlobContainerItem>>. This
    enhancement provides more detailed metadata for each container.

--- Services/Azure/IBlobStorageService.cs
+++ Services/Azure/IBlobStorageService.cs
@@ -14,7 +14,7 @@ namespace PersonalWebApi.Services.Azure
         Task<Uri> UploadFromUriToTemp(string fileUri, string fileName, double ttlInDays, bool overwrite = false, Dictionary<string, string>? metadata = null);
         Task<Uri> UploadFromUriToLibrary(string fileUri, string fileName, bool overwrite = false, Dictionary<string, string>? metadata = null);
         Task<List<BlobItem>> GetFilesWithMetadataAsync(string containerName);
-        Task<List<string>> GetContainersAsync();
+        Task<List<BlobContainerItem>> GetContainersAsync();
         Task<Stream> DownloadFileAsync(Uri fileUri);
     }
 }
