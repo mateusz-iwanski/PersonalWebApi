f55d208d51096f22fef547b2d7cf18df427f71b5
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-16 09:17:55 +0100

    Add experimental NextSteps class to FileStorage service
    Introduce NextSteps class in PersonalWebApi.Services.FileStorage namespace.
    - Marked as experimental with [Experimental("SKEXP0080")].
    - Inherits from KernelProcessStep.
    - Adds ExecuteAsync method to write a debug message.
    - Includes necessary using directives and references to Semantic Kernel documentation.

---
+++ Services/FileStorage/NextSteps.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.SemanticKernel;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PersonalWebApi.Services.FileStorage
+{
+    // https://github.com/microsoft/semantic-kernel/blob/39934f5fa338141c8a64de96895a4e1f440638d7/dotnet/samples/GettingStartedWithProcesses/README.md
+    // https://www.linkedin.com/pulse/introducing-semantic-kernel-process-library-new-era-ai-latorre-g8tef
+    // https://github.com/microsoft/semantic-kernel/blob/39934f5fa338141c8a64de96895a4e1f440638d7/dotnet/samples/GettingStartedWithProcesses/Step00/Steps/DoSomeWorkStep.cs
+    [Experimental("SKEXP0080")]
+    public sealed class NextSteps : KernelProcessStep
+    {
+        [KernelFunction]
+        public async ValueTask ExecuteAsync(KernelProcessStepContext context)
+        {
+            Debug.WriteLine("Step 2 - Doing Some Work...\n");
+        }
+    }
+}
