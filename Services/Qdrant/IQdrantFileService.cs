51245f060be97f906d7a2954c0e480d26b6cea97
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-16 09:18:56 +0100

    Add SearchAsync method and update Qdrant utilities
    - Added `using` directive for `PersonalWebApi.Utilities.Utilities.Qdrant`
    - Moved `using` directive for `Qdrant.Client.Grpc`
    - Commented out `Setup` method in `IQdrantFileService` interface
    - Added `SearchAsync` method to `IQdrantFileService` interface
    - `SearchAsync` method supports queries, optional filters, and a limit

--- Services/Qdrant/IQdrantFileService.cs
+++ Services/Qdrant/IQdrantFileService.cs
@@ -1,4 +1,5 @@
-﻿using Qdrant.Client.Grpc;
+﻿using PersonalWebApi.Utilities.Utilities.Qdrant;
+using Qdrant.Client.Grpc;
 using System.Security.Claims;
 using static PersonalWebApi.Services.Services.Qdrant.QdrantFileService;
 
@@ -6,18 +7,19 @@ namespace PersonalWebApi.Services.Services.Qdrant
 {
     public interface IQdrantFileService
     {
-        void Setup(
-            string modelEmbedding,
-            string modelEmbeddingApiKey,
-            string qdrantUri,
-            string qdrantApiKey,
-            string qdrantCollectionName,
-            Distance qdrantCollectionDistance,
-            ulong qdrantCollectionSize,
-            bool overwrite,
-            ClaimsPrincipal user
-            );
+        //void Setup(
+        //    string modelEmbedding,
+        //    string modelEmbeddingApiKey,
+        //    string qdrantUri,
+        //    string qdrantApiKey,
+        //    string qdrantCollectionName,
+        //    Distance qdrantCollectionDistance,
+        //    ulong qdrantCollectionSize,
+        //    bool overwrite,
+        //    ClaimsPrincipal user
+        //    );
 
         Task<Guid> AddAsync(IFormFile document, Guid conversationUuid, int maxTokensPerLine, int maxSummaryCharacters);
+        Task<List<QdrantFileSearchResultType>> SearchAsync(List<string> queries, Dictionary<string, string> filter = null, int limit = 5);
     }
 }
