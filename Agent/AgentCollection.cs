d83d5098d4ece76f4a194096d1e96c10b0aaf5df
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-16 09:15:47 +0100

    Add AgentCollection class with OpenAI integration
    Introduce AgentCollection class in PersonalWebApi.Agent namespace.
    Add necessary using directives for Azure.Identity, Microsoft.SemanticKernel,
    PersonalWebApi.Agent.SemanticKernel.Plugins.DataGathererPlugin, and
    PersonalWebApi.Exceptions. Define private _kernelBuilder and readonly
    _configuration fields. Add AssistantConfiguration record for configuration
    details. Initialize _kernelBuilder and _configuration in the constructor.
    Implement AddOpenAIChatCompletion method to add OpenAI chat completion model
    to the kernel builder. Define OpenAiSettings property to fetch OpenAI settings
    from the configuration and handle missing settings with SettingsException.

---
+++ Agent/AgentCollection.cs
@@ -0,0 +1,35 @@
+﻿using Azure.Identity;
+using Microsoft.SemanticKernel;
+using PersonalWebApi.Agent.SemanticKernel.Plugins.DataGathererPlugin;
+using PersonalWebApi.Exceptions;
+
+namespace PersonalWebApi.Agent
+{
+    public class AgentCollection
+    {
+        private IKernelBuilder _kernelBuilder { get; set; }
+        private readonly IConfiguration _configuration;
+        
+        private record AssistantConfiguration(string key, string defaultModelId, string? endpoint, string? deploymentName); 
+
+        public AgentCollection(IConfiguration configuration)
+        {
+            _kernelBuilder = Kernel.CreateBuilder();
+            _configuration = configuration;
+        }
+
+        public Kernel AddOpenAIChatCompletion(string? chatModelId = null) => _kernelBuilder.AddOpenAIChatCompletion(chatModelId ?? OpenAiSettings.defaultModelId, OpenAiSettings.key).Build();
+
+        #region assistant settings
+
+        private AssistantConfiguration OpenAiSettings { get => new AssistantConfiguration(
+            _configuration.GetSection("OpenAI:Access:ApiKey").Value ?? throw new SettingsException("OpenAI:Access:ApiKey not exists"),
+            _configuration.GetSection("OpenAI:DefaultModelId").Value ?? throw new SettingsException("OpenAI:DefaultModelId not exists"),
+            null,
+            null
+            );
+        }
+
+        #endregion
+    }
+}
