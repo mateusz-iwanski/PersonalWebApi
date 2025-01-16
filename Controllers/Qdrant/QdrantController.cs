12a07a272bfbdb32de2dae2a5db96c2ffe451d64
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-16 09:16:26 +0100

    Refactor QdrantController to remove config dependencies
    Removed retrieval of various configuration settings from the
    InsertDocumentToQdrant method in QdrantController. This includes
    removing settings related to Qdrant file collection and OpenAI
    API keys. The setup of the _qdrant object with these values has
    also been removed. The maxTokenFileChunked variable is no longer
    retrieved, but maxSummaryFileCharacters is still parsed from the
    configuration. The call to _qdrant.AddAsync remains without the
    previous setup.

--- Controllers/Qdrant/QdrantController.cs
+++ Controllers/Qdrant/QdrantController.cs
@@ -41,45 +41,11 @@ namespace PersonalWebApi.Controllers.Controllers.Qdrant
         [HttpPost("{converationId:guid}/file/add")]
         public async Task<string> InsertDocumentToQdrant(IFormFile document, Guid converationId)
         {
-
-            var collectionName = _configuration.GetSection("Qdrant:FileCollection:Name").Value ??
-               throw new SettingsException("Qdrant:FileCollection:Name not exists in appsettings");
-
-            var collectionDistance = _configuration.GetSection("Qdrant:FileCollection:Distance").Value ??
-                throw new SettingsException("Qdrant:FileCollection:Distance not exists in appsettings");
-
-            var collectionSize = ulong.Parse(_configuration.GetSection("Qdrant:FileCollection:Size").Value ??
-                throw new SettingsException("Qdrant:FileCollection:Size not exists in appsettings"));
-
-            var modelEmbedding = _configuration.GetSection("Qdrant:FileCollection:OpenAiModelEmbedding").Value ??
-                throw new SettingsException("Qdrant:FileCollection:OpenAiModelEmbedding not exists in appsettings");
-
-            var modelApiKey = _configuration.GetSection("OpenAI:Access:ApiKey").Value ??
-                throw new SettingsException("OpenAI:Access:ApiKey not exists in appsettings");
-
-            var qdrantApiKey = _configuration.GetSection("Qdrant:Access:Key").Value ??
-                throw new SettingsException("Qdrant:Access:Key not exists in appsettings");
-
-            var qdrantUrl = _configuration.GetSection("Qdrant:Access:Uri").Value ??
-                throw new SettingsException("Qdrant:Access:Uri not exists in appsettings");
-
             var maxTokenFileChunked = _configuration.GetSection("Qdrant:MaxTokenFileChunked").Value ??
                 throw new SettingsException("Qdrant:MaxTokenFileChunked not exists in appsettings");
 
             var maxSummaryFileCharacters = int.Parse(_configuration.GetSection("Qdrant:MaxSummaryFileCharacters").Value ??
                 throw new SettingsException("Qdrant:MaxSummaryFileCharacters not exists in appsettings"));
-
-            _qdrant.Setup(
-                modelEmbedding: modelEmbedding,
-                modelEmbeddingApiKey: modelApiKey,
-                qdrantUri: qdrantUrl,
-                qdrantApiKey: qdrantApiKey,
-                qdrantCollectionName: collectionName,
-                qdrantCollectionDistance: Distance.Cosine,
-                qdrantCollectionSize: collectionSize,
-                overwrite: true,
-                user: User
-                );
 
             var fileUUID = await _qdrant.AddAsync(
                 document: document,
