bdc2ff59726a76207ad5e9c5b52691f1058bd82b
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-17 08:44:28 +0100

    Add DocumentReaderStep for reading URIs and emitting events
    Introduced new functionality for reading documents from a URI using
    a document reader service and emitting an event with the read text.
    This includes:
    - New namespace `PersonalWebApi.Services.Qdrant.Processes.Steps`
    - Static classes `DocumentReaderStepFunctions` and `DocumentReaderStepOutputEvents`
    - `DocumentReaderStep` class with `[Experimental("SKEXP0080")]` attribute
    - Asynchronous method `ReadUriAsync` in `DocumentReaderStep`
    - Necessary using directives for required namespaces

---
+++ Services/Qdrant/Processes/Steps/DocumentReaderStep.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.SemanticKernel;
+using PersonalWebApi.Services.FileStorage;
+using PersonalWebApi.Utilities.Utilities.DocumentReaders;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PersonalWebApi.Services.Qdrant.Processes.Steps
+{
+    public static class DocumentReaderStepFunctions
+    {
+        public const string ReadUri = nameof(ReadUri);
+    }
+
+    public static class DocumentReaderStepOutputEvents
+    {
+        public const string Readed = nameof(Readed);
+    }
+
+    [Experimental("SKEXP0080")]
+    public sealed class DocumentReaderStep : KernelProcessStep
+    {
+        [KernelFunction(DocumentReaderStepFunctions.ReadUri)]
+        public async ValueTask ReadUriAsync(KernelProcessStepContext context, Kernel kernel, Uri uri)
+        {
+            var documentReader = kernel.GetRequiredService<IDocumentReaderDocx>();
+            var text = await documentReader.ReadAsync(uri);
+
+            await context.EmitEventAsync(new() { Id = DocumentReaderStepOutputEvents.Readed, Data = text });
+        }
+    }
+}
