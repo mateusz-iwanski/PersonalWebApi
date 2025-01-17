a88707d9d8e207fe51ad4bd2ca79243afa279a6e
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-17 08:45:35 +0100

    Add TextChunkerStep class for text chunking operations
    Introduce TextChunkerStep class in PersonalWebApi.Services.Qdrant.Processes.Steps namespace for handling text chunking using SemanticKernelTextChunker. Add necessary using directives. Define TextChunkerStepItem record for chunking parameters. Add static classes for function and event name constants. Mark TextChunkerStep as experimental with SKEXP0080 attribute. Inherit from KernelProcessStep and initialize _textChunker field. Implement ChunkTextAsync method with KernelFunction attribute for asynchronous text chunking and event emission.

---
+++ Services/Qdrant/Processes/Steps/TextChunkerStep.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.SemanticKernel;
+using PersonalWebApi.Utilities.Utilities.DocumentReaders;
+using PersonalWebApi.Utilities.Utilities.Models;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PersonalWebApi.Services.Qdrant.Processes.Steps
+{
+    public record TextChunkerStepItem(string ConversationId, string Text, int MaxTokensPerLine, string ModelEmbedding);
+
+    public static class TextChunkerStepFunctions
+    {
+        public const string ChunkText = nameof(ChunkText);
+    }
+
+    public static class TextChunkerStepOutputEvents
+    {
+        public const string Chunked = nameof(Chunked);
+    }
+
+    [Experimental("SKEXP0080")]
+    public sealed class TextChunkerStep : KernelProcessStep
+    {
+        private readonly SemanticKernelTextChunker _textChunker;
+
+        public TextChunkerStep(SemanticKernelTextChunker textChunker)
+        {
+            _textChunker = textChunker;
+        }
+
+        [KernelFunction(TextChunkerStepFunctions.ChunkText)]
+        public async ValueTask ChunkTextAsync(KernelProcessStepContext context, Kernel kernel, TextChunkerStepItem textChunkerStepItem)
+        {
+            var chunker = new SemanticKernelTextChunker(textChunkerStepItem.ModelEmbedding);
+            var chunks = _textChunker.ChunkText(textChunkerStepItem.ConversationId, textChunkerStepItem.MaxTokensPerLine, textChunkerStepItem.Text);
+
+            await context.EmitEventAsync(new() { Id = TextChunkerStepOutputEvents.Chunked, Data = chunks });
+        }
+    }
+}
