f397c61e569c03d60e859a5eeb554b9adda86fe9
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-17 22:55:16 +0100

    Add DocumentReaderBase class with author extraction
    Introduce DocumentReaderBase class in PersonalWebApi.Utilities.Utilities.DocumentReaders namespace. This class depends on IFileStorageService, injected via constructor. Add static method GetAuthorNameFromDocument to extract author's name from Wordprocessing document's core properties. Include necessary using directives for DocumentFormat.OpenXml.Packaging and PersonalWebApi.Services.FileStorage.

---
+++ Utilities/Document/DocumentReaderBase.cs
@@ -0,0 +1,27 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using PersonalWebApi.Services.FileStorage;
+
+namespace PersonalWebApi.Utilities.Utilities.DocumentReaders
+{
+    public class DocumentReaderBase
+    {
+        protected readonly IFileStorageService _blobService;
+
+        public DocumentReaderBase(IFileStorageService blobService)
+        {
+            _blobService = blobService;
+        }
+
+        public static string GetAuthorNameFromDocument(IFormFile document)
+        {
+            using (var stream = document.OpenReadStream())
+            {
+                using (var wordDocument = WordprocessingDocument.Open(stream, false))
+                {
+                    var coreProperties = wordDocument.PackageProperties;
+                    return coreProperties.Creator;
+                }
+            }
+        }
+    }
+}
