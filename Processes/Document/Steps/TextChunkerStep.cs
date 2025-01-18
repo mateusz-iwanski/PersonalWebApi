2751888223db601d51d3ada7304f8f139b1804ac
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-18 22:35:11 +0100

    Add experimental TextChunkerStep class and related functions
    Introduce TextChunkerStep class in PersonalWebApi.Services.Qdrant.Processes.Steps namespace, inheriting from KernelProcessStep. Add TextChunkerStepFunctions with ChunkText constant. Implement ChunkTextAsync method in TextChunkerStep, using a text chunker service to process text and emit KernelProcessEvent with DocumentEvents.Chunked. Import necessary namespaces for functionality.

---
+++ Processes/Document/Steps/TextChunkerStep.cs
@@ -0,0 +1,38 @@
+﻿using Azure.AI.FormRecognizer.DocumentAnalysis;
+using DocumentFormat.OpenXml.Office2013.Drawing.ChartStyle;
+using Microsoft.SemanticKernel;
+using PersonalWebApi.Processes.Document.Events;
+using PersonalWebApi.Processes.Document.Models;
+using PersonalWebApi.Processes.Qdrant.Models;
+using PersonalWebApi.Services.Agent;
+using PersonalWebApi.Utilities.Utilities.DocumentReaders;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PersonalWebApi.Services.Qdrant.Processes.Steps
+{
+    public static class TextChunkerStepFunctions
+    {
+        public const string ChunkText = nameof(ChunkText);
+    }
+
+    [Experimental("SKEXP0080")] 
+    public sealed class TextChunkerStep : KernelProcessStep
+    {
+
+        [KernelFunction(TextChunkerStepFunctions.ChunkText)]
+        public async ValueTask ChunkTextAsync(KernelProcessStepContext context, Kernel kernel, DocumentStepDto documentStepDto)
+        {
+            var chunker = kernel.GetRequiredService<ITextChunker>();
+            chunker.Setup("text-embedding-3-small");
+
+            documentStepDto.ChunkerCollection = chunker.ChunkText(100, documentStepDto.Content);
+            
+            await context.EmitEventAsync(
+                new KernelProcessEvent() 
+                { 
+                    Id = DocumentEvents.Chunked, 
+                    Data = documentStepDto
+                });
+        }
+    }
+}
