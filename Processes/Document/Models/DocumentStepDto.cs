68604b9a6411b4668ed02655692a50b31361ace1
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-18 22:34:18 +0100

    Add DocumentStepDto class for document step data transfer
    Introduce DocumentStepDto in PersonalWebApi.Processes.Document.Models.
    This class represents a document step DTO with properties such as
    ConversationUuid, SessionUuid, FileId, Content, Metadata, iFormFile,
    Overwrite, Uri, ChunkerCollection, and Events. A constructor is added
    to initialize the class with fileId, iFormFile, conversationUuid, and
    sessionUuid. Also, include using directives for Qdrant.Models and
    Agent services.

---
+++ Processes/Document/Models/DocumentStepDto.cs
@@ -0,0 +1,48 @@
+﻿using PersonalWebApi.Processes.Qdrant.Models;
+using PersonalWebApi.Services.Agent;
+
+namespace PersonalWebApi.Processes.Document.Models
+{
+    /// <summary>
+    /// This class represents a document step data transfer object.
+    /// It is used to transfer document step data between processes.
+    /// </summary>
+    public class DocumentStepDto
+    {
+        public Guid ConversationUuid { get; set; }
+        public Guid SessionUuid { get; set; }
+
+        public Guid FileId { get; set; }
+        public string Content { get; set; } = string.Empty;
+
+        /// <summary>
+        /// For collecting information about file, content, etc.
+        /// </summary>
+        public Dictionary<string, string> Metadata { get; set; } = new Dictionary<string, string>();
+
+        /// <summary>
+        /// This field is using on input
+        /// </summary>
+        public IFormFile? iFormFile { get; set; } = default;
+
+        /// <summary>
+        /// This field is using on input
+        /// TODO: Make - Create Policy about overwriting file with the same name
+        /// </summary>
+        public bool Overwrite { get; set; } = false;
+
+        public Uri Uri { get; set; } = new Uri("http://example.com");
+
+        public List<DocumentChunkerDto> ChunkerCollection = new List<DocumentChunkerDto>();
+
+        public List<string> Events = new List<string>();  // for example uploaded on external server, etc.
+
+        public DocumentStepDto(Guid fileId, IFormFile iFormFile, Guid conversationUuid, Guid sessionUuid)
+        {
+            FileId = fileId;
+            this.iFormFile = iFormFile;
+            ConversationUuid = conversationUuid;
+            SessionUuid = sessionUuid;
+        }
+    }
+}
