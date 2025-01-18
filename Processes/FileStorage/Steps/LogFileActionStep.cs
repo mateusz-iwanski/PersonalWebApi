90f41c5964936aabefe05c9544c8bda384e10f2a
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-18 22:39:03 +0100

    Add experimental LogFileActionStep class and related methods
    Introduce LogFileActionStep in FileStorage.Steps namespace, marked as experimental with [Experimental("SKEXP0080")]. Add LogFileActionStepFunctions with SaveActionLog constant. Implement SaveActionLogAsync method in LogFileActionStep, decorated with [KernelFunction(LogFileActionStepFunctions.SaveActionLog)]. Method retrieves services, processes UUIDs, creates and saves StorageEventsDto, and emits ActionLogSaved event.

---
+++ Processes/FileStorage/Steps/LogFileActionStep.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using MongoDB.Driver;
+using PersonalWebApi.Models.Storage;
+using PersonalWebApi.Processes.Document.Models;
+using PersonalWebApi.Processes.FileStorage.Events;
+using PersonalWebApi.Processes.FileStorage.Models;
+using PersonalWebApi.Processes.Metadata.Steps;
+using PersonalWebApi.Services.Services.History;
+using PersonalWebApi.Services.Services.System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PersonalWebApi.Processes.FileStorage.Steps
+{
+    public static class LogFileActionStepFunctions
+    {
+        public const string SaveActionLog = nameof(SaveActionLog);
+    }
+
+    [Experimental("SKEXP0080")]
+    public class LogFileActionStep : KernelProcessStep
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="kernel"></param>
+        /// <param name="content"></param>
+        /// <returns>Null</returns>
+        /// <remarks>
+        /// 
+        /// Required service from the provider:
+        ///     - IAssistantHistoryManager
+        ///     - IHttpContextAccessor
+        /// 
+        /// </remarks>
+        [KernelFunction(LogFileActionStepFunctions.SaveActionLog)]
+        public async ValueTask SaveActionLogAsync(KernelProcessStepContext context, Kernel kernel, DocumentStepDto documentStepDto)
+        {
+            var assistantHistoryManager = kernel.GetRequiredService<IAssistantHistoryManager>();
+            var httpContextAccessor = kernel.GetRequiredService<IHttpContextAccessor>();
+
+            (Guid conversationUuid, Guid sessionUuid) = ContextAccessorReader.RetrieveCrucialUuid(httpContextAccessor);
+
+            var storageEvent = new StorageEventsDto(conversationUuid, sessionUuid, documentStepDto.FileId)
+            {
+                EventName = "upload",
+                ServiceName = "fileStore",
+                IsSuccess = true,
+                ActionType = "upload",
+                FileUri = documentStepDto.Uri.ToString(),
+            };
+
+            await assistantHistoryManager.SaveAsync(storageEvent);
+
+            await context.EmitEventAsync(new() { Id = FileEvents.ActionLogSaved , Data = documentStepDto });
+        }
+
+    }
+}
