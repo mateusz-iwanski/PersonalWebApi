ca58f76dc0f88263d58e7d733fa385fc9e9eecde
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-18 22:36:22 +0100

    Add experimental FileMetadataComposer class
    Introduce FileMetadataComposer in PersonalWebApi.Processes.FileStorage.Steps namespace, marked as experimental with [Experimental("SKEXP0080")]. Add static class FileMetadataComposerFunction with constant string Collect. FileMetadataComposer inherits from KernelProcessStep and includes CollectAsync method, marked with [KernelFunction(FileMetadataComposerFunction.Collect)]. CollectAsync checks for null iFormFile, adds metadata properties and headers to Metadata dictionary, and emits MetadataCollected event. Add necessary using directives.

---
+++ Processes/FileStorage/Steps/FileMetadataComposer.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Cosmos.Linq;
+using Microsoft.SemanticKernel;
+using PersonalWebApi.Processes.Document.Models;
+using PersonalWebApi.Processes.FileStorage.Events;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PersonalWebApi.Processes.FileStorage.Steps
+{
+    public static class FileMetadataComposerFunction
+    {
+        public const string Collect = nameof(Collect);
+    }
+
+    [Experimental("SKEXP0080")]
+    public class FileMetadataComposer : KernelProcessStep
+    {
+        [KernelFunction(FileMetadataComposerFunction.Collect)]
+        public async ValueTask CollectAsync(KernelProcessStepContext context, Kernel kernel, DocumentStepDto documentStepDto)
+        {
+            if (documentStepDto.iFormFile is null)
+            {
+                throw new System.ArgumentNullException(nameof(documentStepDto.iFormFile));
+            }
+
+            documentStepDto.Metadata.Add("Name", documentStepDto.iFormFile.FileName);
+            documentStepDto.Metadata.Add("ContentType", documentStepDto.iFormFile.ContentType);
+            documentStepDto.Metadata.Add("Length", documentStepDto.iFormFile.Length.ToString());
+            documentStepDto.Metadata.Add("FileName", documentStepDto.iFormFile.FileName);
+            documentStepDto.Metadata.Add("ContentDisposition", documentStepDto.iFormFile.ContentDisposition);
+            documentStepDto.Metadata.Add("Header", documentStepDto.iFormFile.Headers?.ToString() ?? "");
+            documentStepDto.Metadata.Add("HashCode", documentStepDto.iFormFile.GetHashCode().ToString());
+            documentStepDto.Metadata.Add("UploadedAt", DateTime.UtcNow.ToString("o"));
+            documentStepDto.Metadata.Add("IFomrFile", "True");
+            documentStepDto.Metadata.Add("HeaderName", documentStepDto.iFormFile.Name);
+
+            // Add all headers to metadata
+            if (documentStepDto.iFormFile.Headers != null)
+            {
+                foreach (var header in documentStepDto.iFormFile.Headers)
+                {
+                    documentStepDto.Metadata.Add($"Header-{header.Key}", header.Value.ToString());
+                }
+            }
+            await context.EmitEventAsync(
+                new()
+                {
+                    Id = FileEvents.MetadataCollected,
+                    Data = documentStepDto
+                });
+
+        }
+
+    }
+}
