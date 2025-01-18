3d7f1f2e5c1858ca672513d27e7924eaded2907e
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-18 22:35:39 +0100

    Add FileStorageStepDto class in new namespace
    Introduced a new namespace `PersonalWebApi.Processes.FileStorage.Models`.
    Added `FileStorageStepDto` class with properties:
    - `FileName` (string, initialized to empty string)
    - `FileId` (Guid, initialized to Guid.Empty)
    - `FileUri` (Uri)
    - `ErrorMessage` (string, initialized to empty string)
    - `IsSuccess` (bool, initialized to true)

---
+++ Processes/FileStorage/Models/FileStorageStepDto.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonalWebApi.Processes.FileStorage.Models
+{
+    public class FileStorageStepDto
+    {
+        public string FileName { get; set; } = string.Empty;
+        public Guid FileId { get; set; } = Guid.Empty;
+        public Uri FileUri { get; set; }
+        public string ErrorMessage { get; set; } = string.Empty;
+        public bool IsSuccess { get; set; } = true;
+    }
+
+}
