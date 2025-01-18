be1d48e267f0d4658d29e17a75cdf247b347e1b1
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-18 22:32:50 +0100

    Refactor AgentCollection to AgentRouter with DI enhancements
    Removed AgentCollection class and added AgentRouter class with similar functionality and additional service registrations. Moved AddOpenAIChatCompletion method and AssistantConfiguration record to AgentRouter. Included dependency injection for IConfiguration and HttpContextAccessor.

---
+++ Agent/AgentRouter.cs
@@ -0,0 +1,57 @@
+﻿using Azure.Identity;
+using Microsoft.SemanticKernel;
+using PersonalWebApi.Agent.SemanticKernel.Observability;
+using PersonalWebApi.Agent.SemanticKernel.Plugins.DataGathererPlugin;
+using PersonalWebApi.Exceptions;
+using PersonalWebApi.Services.FileStorage;
+using PersonalWebApi.Services.NoSQLDB;
+using PersonalWebApi.Services.Services.Agent;
+using PersonalWebApi.Services.Services.History;
+using PersonalWebApi.Services.Services.Qdrant;
+using PersonalWebApi.Utilities.Utilities.DocumentReaders;
+
+namespace PersonalWebApi.Agent
+{
+    public class AgentRouter
+    {
+        private IKernelBuilder _kernelBuilder { get; set; }
+        private readonly IConfiguration _configuration;
+        
+        private record AssistantConfiguration(string key, string defaultModelId, string? endpoint, string? deploymentName); 
+
+        public AgentRouter(IConfiguration configuration)
+        {
+            _kernelBuilder = Kernel.CreateBuilder();
+            
+            _kernelBuilder.Services.AddSingleton<IConfiguration>(configuration);
+            _kernelBuilder.Services.AddHttpContextAccessor();
+            // register services
+
+            _kernelBuilder.Services.AddScoped<IEmbedding, EmbeddingOpenAi>();
+            _kernelBuilder.Services.AddScoped<IQdrantService, QdrantService>();
+            _kernelBuilder.Services.AddScoped<IFileStorageService, AzureBlobStorageService>();
+            _kernelBuilder.Services.AddScoped<IDocumentReaderDocx, DocumentReaderDocx>();
+            _kernelBuilder.Services.AddScoped<INoSqlDbService, AzureCosmosDbService>();
+            _kernelBuilder.Services.AddScoped<IAssistantHistoryManager, AssistantHistoryManager>();
+
+            // Add the RenderedPromptFilterHandler as a service
+            _kernelBuilder.Services.AddScoped<IPromptRenderFilter, RenderedPromptFilterHandler>();
+
+            _configuration = configuration;
+        }
+
+        public Kernel AddOpenAIChatCompletion(string? chatModelId = null) => _kernelBuilder.AddOpenAIChatCompletion(chatModelId ?? OpenAiSettings.defaultModelId, OpenAiSettings.key).Build();
+
+        #region assistant settings
+
+        private AssistantConfiguration OpenAiSettings { get => new AssistantConfiguration(
+            _configuration.GetSection("OpenAI:Access:ApiKey").Value ?? throw new SettingsException("OpenAI:Access:ApiKey not exists"),
+            _configuration.GetSection("OpenAI:DefaultModelId").Value ?? throw new SettingsException("OpenAI:DefaultModelId not exists"),
+            null,
+            null
+            );
+        }
+
+        #endregion
+    }
+}
