01ae4ab26173ee926c849932e09d0d63a96af1c7
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-18 22:34:29 +0100

    Add experimental DocumentReaderStep class with ReadUriAsync
    Introduce DocumentReaderStep class in PersonalWebApi.Services.Qdrant.Processes.Steps namespace, marked as experimental with [Experimental("SKEXP0080")]. This class inherits from KernelProcessStep and includes the ReadUriAsync method, which reads document content from a URI using IDocumentReaderDocx and emits a DocumentEvents.Readed event. Add necessary using directives and a new static class DocumentReaderStepFunctions for function name constants.

---
+++ Processes/Document/Steps/DocumentReaderStep.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.SemanticKernel;
+using PersonalWebApi.Processes.Document.Events;
+using PersonalWebApi.Processes.Document.Models;
+using PersonalWebApi.Processes.FileStorage.Models;
+using PersonalWebApi.Processes.FileStorage.Steps;
+using PersonalWebApi.Services.FileStorage;
+using PersonalWebApi.Utilities.Utilities.DocumentReaders;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PersonalWebApi.Services.Qdrant.Processes.Steps
+{
+    public static class DocumentReaderStepFunctions
+    {
+        public const string ReadUri = nameof(ReadUri);
+        public const string PrintIntroMessage = nameof(PrintIntroMessage);
+    }
+
+
+    [Experimental("SKEXP0080")]
+    public sealed class DocumentReaderStep : KernelProcessStep
+    {
+        [KernelFunction(DocumentReaderStepFunctions.ReadUri)]
+        public async ValueTask ReadUriAsync(KernelProcessStepContext context, Kernel kernel, DocumentStepDto documentStepDto)
+        {
+            var documentReader = kernel.GetRequiredService<IDocumentReaderDocx>();
+            var text = await documentReader.ReadAsync(documentStepDto.Uri);
+
+            documentStepDto.Content = text;
+
+            await context.EmitEventAsync(new() { Id = DocumentEvents.Readed, Data = documentStepDto });
+        }
+    }
+}
