a1a35752c38c2172dbe96cee993624ce19ebcbf4
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-18 22:36:36 +0100

    Add FileStorageStep class for file upload handling
    Introduce FileStorageStep class in PersonalWebApi.Processes.FileStorage.Steps namespace to handle file storage operations. Add necessary using directives and FileStorageFunctions static class with UploadIFormFile constant. Mark FileStorageStep as experimental with SKEXP0080 attribute. Implement UploadIFormFileAsync method to upload files using IFileStorageService and IConfiguration, set file URI, and emit upload events.

---
+++ Processes/FileStorage/Steps/FileStorageStep.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.SemanticKernel;
+using PersonalWebApi.Exceptions;
+using PersonalWebApi.Models.FileStorage;
+using PersonalWebApi.Processes.Document.Models;
+using PersonalWebApi.Processes.FileStorage.Events;
+using PersonalWebApi.Processes.FileStorage.Models;
+using PersonalWebApi.Services.FileStorage;
+using PersonalWebApi.Services.Qdrant.Processes.Steps;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PersonalWebApi.Processes.FileStorage.Steps
+{
+    public static class FileStorageFunctions
+    {
+        public const string UploadIFormFile = nameof(UploadIFormFile);
+    }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <return>Uri of uploaded file</return>
+    [Experimental("SKEXP0080")]
+    public sealed class FileStorageStep : KernelProcessStep
+    {
+        [KernelFunction(FileStorageFunctions.UploadIFormFile)]
+        public async ValueTask UploadIFormFileAsync(KernelProcessStepContext context, Kernel kernel, DocumentStepDto documentStepItem)
+        {
+            var fileStorageService = kernel.GetRequiredService<IFileStorageService>();
+            var configuration = kernel.GetRequiredService<IConfiguration>();
+
+            fileStorageService.SetContainer(
+                configuration.GetSection("Qdrant:Container:Name").Value ?? throw new SettingsException("Qdrant:Container:Name not exists")
+                );
+
+            var uri = await fileStorageService.UploadToContainerAsync(
+                fileId: documentStepItem.FileId,
+                file: documentStepItem.iFormFile,
+                overwrite: documentStepItem.Overwrite,
+                metadata: documentStepItem.Metadata
+                );
+
+            documentStepItem.Uri = uri;
+            documentStepItem.Events.Add("Uploaded on external server, look on Uri");
+
+            await context.EmitEventAsync(
+                new() 
+                { 
+                    Id = FileEvents.Uploaded, 
+                    Data = documentStepItem
+                });
+        }
+    }
+}
