23eaa01086c8c305f2abfb5bec5148e31064a1eb
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-18 22:42:12 +0100

    Update IQdrantService interface and using directives
    - Updated `using` directives to include `PersonalWebApi.Services.Agent` and rearranged them.
    - Removed commented-out `Setup` method from `IQdrantService` interface.
    - Added `AddAsync` method to `IQdrantService` interface.
    - Changed return type of `SearchAsync` method in `IQdrantService` interface to `Task<List<Dictionary<string, object>>>`.

--- Services/Qdrant/IQdrantService.cs
+++ Services/Qdrant/IQdrantService.cs
@@ -1,4 +1,5 @@
-﻿using PersonalWebApi.Utilities.Utilities.Qdrant;
+﻿using PersonalWebApi.Services.Agent;
+using PersonalWebApi.Utilities.Utilities.Qdrant;
 using Qdrant.Client.Grpc;
 using System.Security.Claims;
 using static PersonalWebApi.Services.Services.Qdrant.QdrantService;
@@ -7,19 +8,8 @@ namespace PersonalWebApi.Services.Services.Qdrant
 {
     public interface IQdrantService
     {
-        //void Setup(
-        //    string modelEmbedding,
-        //    string modelEmbeddingApiKey,
-        //    string qdrantUri,
-        //    string qdrantApiKey,
-        //    string qdrantCollectionName,
-        //    Distance qdrantCollectionDistance,
-        //    ulong qdrantCollectionSize,
-        //    bool overwrite,
-        //    ClaimsPrincipal user
-        //    );
-
+        Task AddAsync(string chunk, Dictionary<string, string> metadata, Guid conversationUuid, Guid fileId);
         Task<Guid> AddAsync(IFormFile document, Guid conversationUuid, int maxTokensPerLine, int maxSummaryCharacters);
-        Task<List<QdrantFileSearchResultType>> SearchAsync(List<string> queries, Dictionary<string, string> filter = null, int limit = 5);
+        Task<List<Dictionary<string, object>>> SearchAsync(List<string> queries, Dictionary<string, string> filter = null, int limit = 5);
     }
 }
