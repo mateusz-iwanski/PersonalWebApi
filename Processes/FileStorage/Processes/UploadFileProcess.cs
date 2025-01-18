26f530b94bec95fc8f55a3c5ee4454f3b9e5fff3
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-18 22:35:55 +0100

    Add UploadFileProcess class with CreateProcess method
    Introduce a new static class `UploadFileProcess` in the `PersonalWebApi.Processes.FileStorage.Processes` namespace. Add necessary using directives. Implement the `CreateProcess` method with `[Experimental("SKEXP0080")]` attribute, initializing a `ProcessBuilder` with steps `FileStorageStep` and `LogFileActionStep`. Set up event handling for `FileEvents.StartProcess` and `FileEvents.Uploaded`. Return the configured `ProcessBuilder` instance.

---
+++ Processes/FileStorage/Processes/UploadFileProcess.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.SemanticKernel;
+using PersonalWebApi.Processes.FileStorage.Events;
+using PersonalWebApi.Processes.FileStorage.Steps;
+using PersonalWebApi.Services.Qdrant.Processes.Steps;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PersonalWebApi.Processes.FileStorage.Processes
+{
+    public static class UploadFileProcess
+    {
+        [Experimental("SKEXP0080")]
+        public static ProcessBuilder CreateProcess(string processName = "FileStorageUploadFile")
+        {
+            var process = new ProcessBuilder(processName);
+
+            var fileStorageStep = process.AddStepFromType<FileStorageStep>();
+            var logFileActionStep = process.AddStepFromType<LogFileActionStep>();
+
+            process.OnInputEvent(FileEvents.StartProcess).SendEventTo(new ProcessFunctionTargetBuilder(fileStorageStep));
+            fileStorageStep.OnEvent(FileEvents.Uploaded).SendEventTo(new ProcessFunctionTargetBuilder(logFileActionStep, parameterName: "stepDataModel"));
+
+            // logFileActionStep -> return FileStorageStepDto
+
+            return process;
+        }
+    }
+}
