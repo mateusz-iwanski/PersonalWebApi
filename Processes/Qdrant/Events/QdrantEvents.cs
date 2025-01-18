4b482f796a496643dfc7d0c1ae05c4a39d612e56
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-18 22:39:27 +0100

    Add QdrantEvents class with event name constants
    Introduced a new namespace `PersonalWebApi.Processes.Qdrant.Events` and added a static class `QdrantEvents` within it. The class contains two constant string fields: `StartProcess` and `EmbeddingAdded`, which serve as event names.

---
+++ Processes/Qdrant/Events/QdrantEvents.cs
@@ -0,0 +1,8 @@
+﻿namespace PersonalWebApi.Processes.Qdrant.Events
+{
+    public static class QdrantEvents
+    {
+        public const string StartProcess = nameof(StartProcess);
+        public const string EmbeddingAdded = nameof(EmbeddingAdded);
+    }
+}
