94c150f1f28d53dd4e6aa6bcd91b8c877e0cc284
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-19 12:35:08 +0100

    Add NoSqlDbEvents class for NoSQL DB event names
    Introduced a new namespace `PersonalWebApi.Processes.NoSQLDB.Events`
    and a static class `NoSqlDbEvents` to centralize event names related
    to NoSQL database processes. This includes two public constant string
    fields: `StartProcess` and `Saved`, to standardize event naming
    throughout the application.

---
+++ Processes/NoSQLDB/Events/NoSqlDbEvents.cs
@@ -0,0 +1,8 @@
+﻿namespace PersonalWebApi.Processes.NoSQLDB.Events
+{
+    public static class NoSqlDbEvents
+    {
+        public const string StartProcess = nameof(StartProcess);
+        public const string Saved = nameof(Saved);
+    }
+}
