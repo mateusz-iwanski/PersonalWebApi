20c796169c7dfa832defc0b1da9c238654ed94ac
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-20 00:59:35 +0100

    Add SpecifyDocumentLanguage method and update SpecifyDocumentType
    Updated using directives to include Newtonsoft.Json. Changed the return attribute description for SpecifyDocumentType method. Modified SpecifyDocumentType to use DocumentTypePlugin instead of TagCollectPlugin. Added SpecifyDocumentLanguage method with KernelFunction and Description attributes, which uses DocumentLanguagePlugin to determine and return the language of the content.

--- Agent/SemanticKernel/Plugins/DataGathererPlugin/DocumentInfoPlugin.cs
+++ Agent/SemanticKernel/Plugins/DataGathererPlugin/DocumentInfoPlugin.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.SemanticKernel;
+using Newtonsoft.Json;
 using System.ComponentModel;
 
 namespace PersonalWebApi.Agent.SemanticKernel.Plugins.DataGathererPlugin
@@ -7,20 +8,36 @@ namespace PersonalWebApi.Agent.SemanticKernel.Plugins.DataGathererPlugin
     {
         [KernelFunction("specify_document_type")]
         [Description("Specify the type of content (e.g., article, essay, report)")]
-        [return: Description("List<string> with tags")]
+        [return: Description("List<string> with types")]
         public async Task<List<string>> SpecifyDocumentType(string textContent, Kernel kernel)
         {
             var prompts = kernel.CreatePluginFromPromptDirectory(Path.Combine(AppContext.BaseDirectory, "Agent/SemanticKernel/Plugins/Prompt"));
 
             string completeMessage = string.Empty;
-            var result = kernel.InvokeStreamingAsync<StreamingChatMessageContent>(prompts["TagCollectPlugin"], new() { { "textContent", textContent }, });
-
-            await foreach (var message in kernel.InvokeStreamingAsync<StreamingChatMessageContent>(prompts["TagCollectPlugin"], new() { { "textContent", textContent }, }))
+            
+            await foreach (var message in kernel.InvokeStreamingAsync<StreamingChatMessageContent>(prompts["DocumentTypePlugin"], new() { { "textContent", textContent }, }))
             {
                 completeMessage += message;
             }
 
             return JsonConvert.DeserializeObject<List<string>>(completeMessage);
+        }
+
+        [KernelFunction("specify_document_language")]
+        [Description("Specify the language of content (e.g., Polish, English, ...)")]
+        [return: Description("string")]
+        public async Task<string> SpecifyDocumentLanguage(string textContent, Kernel kernel)
+        {
+            var prompts = kernel.CreatePluginFromPromptDirectory(Path.Combine(AppContext.BaseDirectory, "Agent/SemanticKernel/Plugins/Prompt"));
+
+            string completeMessage = string.Empty;
+
+            await foreach (var message in kernel.InvokeStreamingAsync<StreamingChatMessageContent>(prompts["DocumentLanguagePlugin"], new() { { "textContent", textContent }, }))
+            {
+                completeMessage += message;
+            }
+
+            return completeMessage;
         }
     }
 }
