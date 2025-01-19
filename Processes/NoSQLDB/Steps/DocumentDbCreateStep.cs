9709645af34c68a8e2b36aa681cd85ed5d2557f6
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-19 12:35:21 +0100

    Add DocumentDbCreateStep class for NoSQL DB operations
    Introduce DocumentDbCreateStep class in PersonalWebApi.Processes.NoSQLDB.Steps namespace, marked as experimental. Add SaveAsync method to handle document saving and event emission. Include necessary using directives and add DocumentDbFunctions static class with Save constant.

---
+++ Processes/NoSQLDB/Steps/DocumentDbCreateStep.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.SemanticKernel;
+using PersonalWebApi.Processes.Document.Models;
+using PersonalWebApi.Processes.NoSQLDB.Events;
+using PersonalWebApi.Services.FileStorage;
+using PersonalWebApi.Services.NoSQLDB;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PersonalWebApi.Processes.NoSQLDB.Steps
+{
+
+    public static class DocumentDbFunctions
+    {
+        public const string Save = nameof(Save);
+    }
+
+    [Experimental("SKEXP0080")]
+    public class DocumentDbCreateStep : KernelProcessStep
+    {
+        [KernelFunction(DocumentDbFunctions.Save)]
+        public async ValueTask SaveAsync(KernelProcessStepContext context, Kernel kernel, DocumentStepDto documentStepDto)
+        {
+            var fileStorageService = kernel.GetRequiredService<INoSqlDbService>();
+
+            await fileStorageService.CreateItemAsync(documentStepDto);
+
+            // Save document to DocumentDB
+            await context.EmitEventAsync(
+                new()
+                {
+                    Id = NoSqlDbEvents.Saved,
+                    Data = documentStepDto
+                });
+        }
+    }
+}
