d548b7b5a862ed9425e1bd1736455cded06b387b
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-19 12:35:30 +0100

    Add ChunksTagified constant to QdrantEvents class
    A new constant `ChunksTagified` has been added to the `QdrantEvents`
    class. This constant is intended to be used exclusively within the
    Qdrant pipeline and is not utilized elsewhere.

--- Processes/Qdrant/Events/QdrantEvents.cs
+++ Processes/Qdrant/Events/QdrantEvents.cs
@@ -4,5 +4,8 @@
     {
         public const string StartProcess = nameof(StartProcess);
         public const string EmbeddingAdded = nameof(EmbeddingAdded);
+
+        // it is only available in Qdrant pipeline, not use it anywhere else
+        public const string ChunksTagified = nameof(ChunksTagified);
     }
 }
