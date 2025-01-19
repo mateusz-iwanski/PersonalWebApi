0694420837ada4cc378ddfb0c297dd687067ccd4
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-19 12:35:56 +0100

    Enhance Qdrant pipeline with new steps and event handling
    Added new using directives and steps to the Qdrant pipeline:
    - Added `TagifyStep`, `TagifyChunksStep`, and `SummarizeStep`.
    - Modified event handling for `FileEvents.Uploaded`, `DocumentEvents.Readed`, and `DocumentEvents.Chunked`.
    - Added event handling for `QdrantEvents.ChunksTagified` and `QdrantEvents.EmbeddingAdded`.
    - Added comments about potential issues with the `OnEvent` method.

--- Processes/Qdrant/Pipelines/QdrantPipelines.cs
+++ Processes/Qdrant/Pipelines/QdrantPipelines.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.SemanticKernel;
 using PersonalWebApi.Processes.Document.Events;
 using PersonalWebApi.Processes.Document.Models;
+using PersonalWebApi.Processes.Document.Steps;
 using PersonalWebApi.Processes.FileStorage.Events;
 using PersonalWebApi.Processes.FileStorage.Processes;
 using PersonalWebApi.Processes.FileStorage.Steps;
+using PersonalWebApi.Processes.Metadata.Events;
+using PersonalWebApi.Processes.Metadata.Steps;
+using PersonalWebApi.Processes.NoSQLDB.Steps;
 using PersonalWebApi.Processes.Qdrant.Events;
 using PersonalWebApi.Processes.Qdrant.Processes;
+using PersonalWebApi.Processes.Qdrant.Steps;
 using PersonalWebApi.Services.Qdrant.Processes.Steps;
 using System.Diagnostics.CodeAnalysis;
 
@@ -29,21 +34,33 @@ namespace PersonalWebApi.Processes.Qdrant.Pipelines
             var chunker = process.AddStepFromType<TextChunkerStep>();
             var fileMetadataComposer = process.AddStepFromType<FileMetadataComposer>();
             var qdrantAdd = process.AddStepFromType<QdrantStep>();
-
+            var noSqlDb = process.AddStepFromType<DocumentDbCreateStep>();
+            var tagify = process.AddStepFromType<TagifyStep>();
+            var tagifyChunk = process.AddStepFromType<TagifyChunksStep>();
+            var summarizeDocument = process.AddStepFromType<SummarizeStep>();
 
             process.OnInputEvent(QdrantEvents.StartProcess)
                 .SendEventTo(new ProcessFunctionTargetBuilder(fileStorageStep, functionName: FileStorageFunctions.UploadIFormFile));
             
-            fileStorageStep.OnEvent(FileEvents.Uploaded)
+            fileStorageStep.OnEvent(FileEvents.Uploaded)                
                 .SendEventTo(new ProcessFunctionTargetBuilder(logFileActionStep, functionName: LogFileActionStepFunctions.SaveActionLog, parameterName: "documentStepDto"))
                 .SendEventTo(new ProcessFunctionTargetBuilder(reader, functionName: DocumentReaderStepFunctions.ReadUri, parameterName: "documentStepDto"))
                 .SendEventTo(new ProcessFunctionTargetBuilder(fileMetadataComposer, functionName: FileMetadataComposerFunction.Collect, parameterName: "documentStepDto"));
 
             reader.OnEvent(DocumentEvents.Readed)
-                .SendEventTo(new ProcessFunctionTargetBuilder(chunker, functionName: TextChunkerStepFunctions.ChunkText, parameterName: "documentStepDto"));
+                .SendEventTo(new ProcessFunctionTargetBuilder(chunker, functionName: TextChunkerStepFunctions.ChunkText, parameterName: "documentStepDto"))
+                .SendEventTo(new ProcessFunctionTargetBuilder(tagify, functionName: TagifyStepFunctions.GenerateTags, parameterName: "documentStepDto"));
             //
             chunker.OnEvent(DocumentEvents.Chunked)
+                .SendEventTo(new ProcessFunctionTargetBuilder(tagifyChunk, functionName: TagifyStepFunctions.GenerateChunksTags, parameterName: "documentStepDto"))
+                .SendEventTo(new ProcessFunctionTargetBuilder(summarizeDocument, functionName: SummarizeStepFunctions.SummarizeText, parameterName: "documentStepDto"));
+            
+            tagifyChunk.OnEvent(QdrantEvents.ChunksTagified)
                 .SendEventTo(new ProcessFunctionTargetBuilder(qdrantAdd, functionName: QdrantStepFunctions.AddEmbedding, parameterName: "documentStepDto"));
+
+            qdrantAdd.OnEvent(QdrantEvents.EmbeddingAdded)
+                .SendEventTo(new ProcessFunctionTargetBuilder(noSqlDb, functionName: DocumentDbFunctions.Save, parameterName: "documentStepDto"));
+                
 
 
             // TODO: cos wymyslec, mozliwe ze sam framework dziala nieprawidlowo
