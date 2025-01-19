9fb858643befa1a5577fffb3f16e49c97f191cbf
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-20 00:59:52 +0100

    Refactor TagCollectorPlugin and update method description
    Corrected namespace to TagCollectorPlugin. Updated GenerateTags method description for clarity. Removed unused Kernel property and result variable for code simplification.

--- Agent/SemanticKernel/Plugins/DataGathererPlugin/TagCollectorPlugin.cs
+++ Agent/SemanticKernel/Plugins/DataGathererPlugin/TagCollectorPlugin.cs
@@ -15,14 +15,13 @@ namespace PersonalWebApi.Agent.SemanticKernel.Plugins.DataGathererPlugin
         public TagCollectorPlugin() { }
 
         [KernelFunction("generate_tag_for_text_content")]
-        [Description("Generate collect tag for text content")]
+        [Description("Generate/collect tag for text content")]
         [return: Description("List<string> with tags")]
         public async Task<List<string>> GenerateTags(string textContent, Kernel kernel)
         {
             var prompts = kernel.CreatePluginFromPromptDirectory(Path.Combine(AppContext.BaseDirectory, "Agent/SemanticKernel/Plugins/Prompt"));
 
             string completeMessage = string.Empty;
-            var result = kernel.InvokeStreamingAsync<StreamingChatMessageContent>(prompts["TagCollectPlugin"], new() { { "textContent", textContent }, });
 
             await foreach (var message in kernel.InvokeStreamingAsync<StreamingChatMessageContent>(prompts["TagCollectPlugin"], new() { { "textContent", textContent }, }))
             {
