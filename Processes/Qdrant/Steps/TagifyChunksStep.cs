7aa7c5d8fcc9bab3cdf7f51d587db07a0fbb9e1a
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-19 12:36:18 +0100

    Add TagifyChunksStep class for tagging document chunks
    Introduce new experimental TagifyChunksStep class in the
    PersonalWebApi.Processes.Qdrant.Steps namespace. This class
    inherits from KernelProcessStep and includes a TagifyChunks
    constant string.
    
    Add asynchronous method TagifyChunksAsync to generate tags
    for each chunk in a DocumentStepDto object using
    TagCollectorPlugin. Update each chunk's Tags collection and
    add a "chunks tagified" event to documentStepDto.Events.
    Emit ChunksTagified event with the updated documentStepDto.

---
+++ Processes/Qdrant/Steps/TagifyChunksStep.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel;
+using PersonalWebApi.Agent.SemanticKernel.Plugins.DataGathererPlugin;
+using PersonalWebApi.Processes.Document.Models;
+using PersonalWebApi.Processes.Metadata.Events;
+using System.Diagnostics.CodeAnalysis;
+using PersonalWebApi.Processes.Qdrant.Events;
+
+namespace PersonalWebApi.Processes.Qdrant.Steps
+{
+    /// <summary>
+    /// It is used to tagify chunks in DocumentStepDto.
+    /// Not use this anywhere else.
+    /// </summary>
+    /// <remarks>Use it after chunked</remarks>
+    [Experimental("SKEXP0080")]
+    public sealed class TagifyChunksStep : KernelProcessStep
+    {
+        public const string TagifyChunks = nameof(TagifyChunks);
+
+        [KernelFunction(TagifyStepFunctions.GenerateChunksTags)]
+        public async ValueTask TagifyChunksAsync(KernelProcessStepContext context, Kernel kernel, DocumentStepDto documentStepDto)
+        {
+            var tagifyPlugin = new TagCollectorPlugin();
+
+            foreach(var chunk in documentStepDto.ChunkerCollection)
+            {
+                var tagsCollectionForChunk = await tagifyPlugin.GenerateTags(chunk.Content, kernel);
+                foreach (var tag in tagsCollectionForChunk)
+                    chunk.Tags.Add(tag);
+            }
+
+            documentStepDto.Events.Add("chunks tagified");
+
+            await context.EmitEventAsync(new() { Id = QdrantEvents.ChunksTagified, Data = documentStepDto });
+        }
+    }
+}
