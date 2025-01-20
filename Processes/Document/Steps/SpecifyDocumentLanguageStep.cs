8a353a689dc0f5b79957f83c7da67761764a3e88
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-20 01:02:10 +0100

    Add SpecifyDocumentLanguageStep class with async method
    Introduce SpecifyDocumentLanguageStep class in the
    PersonalWebApi.Processes.Document.Steps namespace, marked as
    experimental with [Experimental("SKEXP0080")]. This class inherits
    from KernelProcessStep and includes the SpecifyDocumentLanguageAsync
    method, decorated with [KernelFunction]. The method determines the
    document language using DocumentInfoPlugin and updates
    documentStepDto with the language and an event. Necessary using
    directives are added.

---
+++ Processes/Document/Steps/SpecifyDocumentLanguageStep.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.SemanticKernel;
+using PersonalWebApi.Agent.SemanticKernel.Plugins.DataGathererPlugin;
+using PersonalWebApi.Processes.Document.Events;
+using PersonalWebApi.Processes.Document.Models;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PersonalWebApi.Processes.Document.Steps
+{
+    public static class DocumentLanguageStepFunctions
+    {
+        public const string SpecifyDocumentLanguage = nameof(SpecifyDocumentLanguage);
+    }
+
+    [Experimental("SKEXP0080")]
+    public sealed class SpecifyDocumentLanguageStep : KernelProcessStep
+    {
+        [KernelFunction(DocumentLanguageStepFunctions.SpecifyDocumentLanguage)]
+        public async ValueTask SpecifyDocumentLanguageAsync(KernelProcessStepContext context, Kernel kernel, DocumentStepDto documentStepDto)
+        {
+            var documentInfo = new DocumentInfoPlugin();
+
+            var docLanguage = await documentInfo.SpecifyDocumentLanguage(documentStepDto.Content, kernel);
+
+            documentStepDto.Language = docLanguage;
+
+            documentStepDto.Events.Add("specify document language");
+
+            await context.EmitEventAsync(new() { Id = DocumentEvents.SpecifiedDocumentLanguage, Data = documentStepDto });
+        }
+    }
+}
