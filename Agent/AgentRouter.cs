6564d4c2bbd0ee9128100734772216254ce982f0
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-20 01:01:39 +0100

    Add new methods and refactor AgentRouter class
    Updated using directives in AgentRouter.cs to include necessary namespaces. Added new record type StepAgentMappingConfiguration. Introduced methods GetStepKernel<T>, ListKernel, and StepAgentMappingSettings. Moved and refactored OpenAiSettings property. Refactored AssistantConfiguration record to include StepAgentMappingConfiguration.

--- Agent/AgentRouter.cs
+++ Agent/AgentRouter.cs
@@ -1,8 +1,12 @@
-﻿using Azure.Identity;
+﻿using Amazon.Runtime.Internal.Transform;
+using Azure.Identity;
+using Elastic.Clients.Elasticsearch.QueryDsl;
 using Microsoft.SemanticKernel;
 using PersonalWebApi.Agent.SemanticKernel.Observability;
 using PersonalWebApi.Agent.SemanticKernel.Plugins.DataGathererPlugin;
 using PersonalWebApi.Exceptions;
+using PersonalWebApi.Processes;
+using PersonalWebApi.Processes.Document.Steps;
 using PersonalWebApi.Services.FileStorage;
 using PersonalWebApi.Services.NoSQLDB;
 using PersonalWebApi.Services.Services.Agent;
@@ -17,7 +21,8 @@ namespace PersonalWebApi.Agent
         private IKernelBuilder _kernelBuilder { get; set; }
         private readonly IConfiguration _configuration;
         
-        private record AssistantConfiguration(string key, string defaultModelId, string? endpoint, string? deploymentName); 
+        private record AssistantConfiguration(string key, string defaultModelId, string? endpoint, string? deploymentName);
+        private record StepAgentMappingConfiguration(string Type, string ModelId);
 
         public AgentRouter(IConfiguration configuration)
         {
@@ -44,12 +49,56 @@ namespace PersonalWebApi.Agent
 
         #region assistant settings
 
-        private AssistantConfiguration OpenAiSettings { get => new AssistantConfiguration(
-            _configuration.GetSection("OpenAI:Access:ApiKey").Value ?? throw new SettingsException("OpenAI:Access:ApiKey not exists"),
-            _configuration.GetSection("OpenAI:DefaultModelId").Value ?? throw new SettingsException("OpenAI:DefaultModelId not exists"),
-            null,
-            null
+        #region assistant assigned to step
+
+        //var agentRouter = new AgentRouter(context.Configuration);
+        //var stepKernel = agentRouter.GetStepKernel<SummarizeStepFunctions>(SummarizeStepFunctions.SummarizeText);
+        public Kernel GetStepKernel<T>(string stepFunctionName) where T : IProcessStepFuntion, new()
+        {
+            var stepFunction = new T();
+            if (!stepFunction.GetFunctionNames().Contains(stepFunctionName))
+            {
+                throw new ArgumentException($"Unsupported step function: {stepFunctionName}", nameof(stepFunctionName));
+            }
+
+            return stepFunctionName switch
+            {
+                SummarizeStepFunctions.SummarizeText => ListKernel(StepAgentMappingSettings(SummarizeStepFunctions.SummarizeText).ModelId),
+                SummarizeStepFunctions.AnotherFunction => AddOpenAIChatCompletion(), // Add appropriate handling for other functions
+                _ => throw new ArgumentException($"Unsupported step function: {stepFunctionName}", nameof(stepFunctionName)),
+            };
+        }
+
+        public Kernel ListKernel(string model)
+        {
+            return model.ToLower() switch
+            {
+                "gpt-4o" => _kernelBuilder.AddOpenAIChatCompletion("gpt-4o", OpenAiSettings.defaultModelId, OpenAiSettings.key).Build(),
+                "gpt-4o-mini" => _kernelBuilder.AddOpenAIChatCompletion("gpt-4o-mini", OpenAiSettings.defaultModelId, OpenAiSettings.key).Build()
+                "gpt-35-turbo" => _kernelBuilder.AddOpenAIChatCompletion("gpt-35-turbo", OpenAiSettings.defaultModelId, OpenAiSettings.key).Build()
+            };
+        }
+
+
+        private AssistantConfiguration OpenAiSettings 
+        { 
+            get => new AssistantConfiguration(
+                _configuration.GetSection("OpenAI:Access:ApiKey").Value ?? throw new SettingsException("OpenAI:Access:ApiKey not exists"),
+                _configuration.GetSection("OpenAI:DefaultModelId").Value ?? throw new SettingsException("OpenAI:DefaultModelId not exists"),
+                null,
+                null
             );
+        }
+
+        private StepAgentMappingConfiguration StepAgentMappingSettings(string stepFunctionName){
+            return stepFunctionName switch
+            {
+                "SummarizeText" =>
+                    new StepAgentMappingConfiguration(
+                       _configuration.GetSection("StepAgentMapping:SummarizeText:Type").Value ?? throw new SettingsException("StepAgentMapping:Type not exists"),
+                       _configuration.GetSection("StepAgentMapping:SummarizeText:ModelId").Value ?? throw new SettingsException("StepAgentMapping:ModelId not exists")
+                    )
+            };
         }
 
         #endregion
