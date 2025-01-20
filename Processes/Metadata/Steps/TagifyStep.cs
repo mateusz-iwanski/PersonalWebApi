714c373b62642527def0473581e8af40673bbd60
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-20 22:56:00 +0100

    Refactor TagifyStep to use new kernel configuration
    Updated using directives and namespace for TagifyStep.cs. Modified GenerateTagsAsync to retrieve kernel configuration from appsettings and use AgentRouter. Changed method call from GenerateTags to GenerateTagsPluginAsync with agent. Loop for adding tags remains unchanged.

--- Processes/Metadata/Steps/TagifyStep.cs
+++ Processes/Metadata/Steps/TagifyStep.cs
@@ -3,9 +3,11 @@ using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 using Newtonsoft.Json;
+using PersonalWebApi.Agent;
 using PersonalWebApi.Agent.SemanticKernel.Plugins.DataGathererPlugin;
 using PersonalWebApi.Controllers.Agent;
 using PersonalWebApi.Processes.Document.Models;
+using PersonalWebApi.Processes.Document.Steps;
 using PersonalWebApi.Processes.Metadata.Events;
 using PersonalWebApi.Processes.Qdrant.Events;
 using System.Diagnostics.CodeAnalysis;
@@ -27,8 +29,12 @@ namespace PersonalWebApi.Processes.Metadata.Steps
         [KernelFunction(TagifyStepFunctions.GenerateTags)]
         public async ValueTask GenerateTagsAsync(KernelProcessStepContext context, Kernel kernel, DocumentStepDto documentStepDto)
         {
+            // get kernel by appsettings.StepAgentMappings
+            var config = kernel.GetRequiredService<IConfiguration>();
+            var agent = new AgentRouter(config).GetStepKernel(TagifyStepFunctions.GenerateTags);
+
             var tagifyPlugin = new TagCollectorPlugin();
-            var result = await tagifyPlugin.GenerateTags(documentStepDto.Content, kernel);
+            var result = await tagifyPlugin.GenerateTagsPluginAsync(documentStepDto.Content, agent);
             
             foreach (var tag in result)
             {
