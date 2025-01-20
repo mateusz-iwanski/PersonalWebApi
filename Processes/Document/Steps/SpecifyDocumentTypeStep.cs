f13210b2b0a2b617b874cbe52d37af33e52bba12
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-20 01:02:24 +0100

    Add SpecifyDocumentTypeStep class and related functionality
    Introduce SpecifyDocumentTypeStep class in PersonalWebApi.Processes.Document.Steps namespace. Add necessary namespaces including Microsoft.KernelMemory and Microsoft.SemanticKernel. Create static class DocumentInfoStepFunctions with a constant string SpecifyDocumentType. Mark SpecifyDocumentTypeStep class as experimental and inherit from KernelProcessStep. Implement SpecifyDocumentTypeAsync method to determine document type using DocumentInfoPlugin and update documentStepDto. Emit event with ID DocumentEvents.SpecifiedDocumentType.

---
+++ Processes/Document/Steps/SpecifyDocumentTypeStep.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.KernelMemory;
+using Microsoft.SemanticKernel;
+using PersonalWebApi.Agent.Memory.Observability;
+using PersonalWebApi.Agent.SemanticKernel.Plugins.DataGathererPlugin;
+using PersonalWebApi.Processes.Document.Events;
+using PersonalWebApi.Processes.Document.Models;
+using PersonalWebApi.Processes.Qdrant.Events;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PersonalWebApi.Processes.Document.Steps
+{
+    public static class DocumentInfoStepFunctions
+    {
+        public const string SpecifyDocumentType = nameof(SpecifyDocumentType);
+    }
+
+    [Experimental("SKEXP0080")]
+    public sealed class SpecifyDocumentTypeStep : KernelProcessStep
+    {
+        [KernelFunction(DocumentInfoStepFunctions.SpecifyDocumentType)]
+        public async ValueTask SpecifyDocumentTypeAsync(KernelProcessStepContext context, Kernel kernel, DocumentStepDto documentStepDto)
+        {
+            var documentInfo = new DocumentInfoPlugin();
+
+            var docType = await documentInfo.SpecifyDocumentType(documentStepDto.Content, kernel);
+
+            foreach (var dType in docType)
+            {
+                documentStepDto.DocumentType.Add(dType);
+            }
+
+            documentStepDto.Events.Add("specify document type");
+
+            await context.EmitEventAsync(new() { Id = DocumentEvents.SpecifiedDocumentType, Data = documentStepDto });
+        }
+    }
+
+}
