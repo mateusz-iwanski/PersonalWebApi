c47be7ed7f5c1d429d0083e4641086aa0d19779b
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-20 22:53:18 +0100

    Refactor TagCollectorPlugin and add text cleaning utility
    - Include PersonalWebApi.Utilities.Document namespace in TagCollectorPlugin.
    - Remove Kernel property and constructor from TagCollectorPlugin.
    - Rename GenerateTags method to GenerateTagsPluginAsync for clarity.
    - Add TextFormatter.CleanResponse utility to clean responses before deserialization.

--- Agent/SemanticKernel/Plugins/DataGathererPlugin/TagCollectorPlugin.cs
+++ Agent/SemanticKernel/Plugins/DataGathererPlugin/TagCollectorPlugin.cs
@@ -3,6 +3,7 @@ using Microsoft.SemanticKernel;
 using Newtonsoft.Json;
 using OllamaSharp;
 using PersonalWebApi.Services.Services.System;
+using PersonalWebApi.Utilities.Document;
 using System.ComponentModel;
 using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
 
@@ -10,14 +11,10 @@ namespace PersonalWebApi.Agent.SemanticKernel.Plugins.DataGathererPlugin
 {
     public class TagCollectorPlugin
     {
-        private Kernel _kernel { get; set; }
-        
-        public TagCollectorPlugin() { }
-
         [KernelFunction("generate_tag_for_text_content")]
         [Description("Generate/collect tag for text content")]
         [return: Description("List<string> with tags")]
-        public async Task<List<string>> GenerateTags(string textContent, Kernel kernel)
+        public async Task<List<string>> GenerateTagsPluginAsync(string textContent, Kernel kernel)
         {
             var prompts = kernel.CreatePluginFromPromptDirectory(Path.Combine(AppContext.BaseDirectory, "Agent/SemanticKernel/Plugins/Prompt"));
 
@@ -28,7 +25,9 @@ namespace PersonalWebApi.Agent.SemanticKernel.Plugins.DataGathererPlugin
                 completeMessage += message;
             }
 
-            return JsonConvert.DeserializeObject<List<string>>(completeMessage);
+            var cleanedMessage = TextFormatter.CleanResponse(completeMessage);
+
+            return JsonConvert.DeserializeObject<List<string>>(cleanedMessage);
         }
     }
 }
