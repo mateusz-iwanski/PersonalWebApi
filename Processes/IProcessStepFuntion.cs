c4a9bebdc6d3c94950029cc54f9eb3bef3cde14f
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-20 01:05:08 +0100

    Add IProcessStepFuntion interface in new namespace
    A new namespace `PersonalWebApi.Processes` has been introduced.
    An interface `IProcessStepFuntion` has been added within this namespace.
    The interface includes a summary documentation comment explaining its purpose, which is to mark a class as a process step function and to assign functions to an Agent, referencing the Agent Router.
    The interface `IProcessStepFuntion` declares a single method `GetFunctionNames` that returns an `IEnumerable<string>`.

---
+++ Processes/IProcessStepFuntion.cs
@@ -0,0 +1,11 @@
+﻿namespace PersonalWebApi.Processes
+{
+    /// <summary>
+    /// Use to mark a class as a process step function.
+    /// Is used to assigned function to Agent, look Agent Router
+    /// </summary>
+    public interface IProcessStepFuntion 
+    {
+        public IEnumerable<string> GetFunctionNames();
+    }
+}
