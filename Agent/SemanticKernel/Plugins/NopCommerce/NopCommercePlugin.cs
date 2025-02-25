f2e969396902fc8898005820bcc49074e4e5024c
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-02-25 11:54:30 +0100

    Add NopCommercePlugin class and necessary using directives
    Introduced a new namespace `PersonalWebApi.Agent.SemanticKernel.Plugins.NopCommerce` and created a new public class `NopCommercePlugin` within it. Added several using directives to the `NopCommercePlugin.cs` file, including `Microsoft.SemanticKernel`, `System.ComponentModel.DataAnnotations`, `System.ComponentModel`, `DocumentFormat.OpenXml.Wordprocessing`, and `PersonalWebApi.Utilities.Document`.

---
+++ Agent/SemanticKernel/Plugins/NopCommerce/NopCommercePlugin.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.SemanticKernel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using DocumentFormat.OpenXml.Wordprocessing;
+using PersonalWebApi.Utilities.Document;
+
+namespace PersonalWebApi.Agent.SemanticKernel.Plugins.NopCommerce
+{
+    public class NopCommercePlugin
+    {
+    }
+}
