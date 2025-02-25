60bea6a645ba75825a9f900d1945d117436ca120
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-02-25 12:35:06 +0100

    Add CollectDataProcess class with experimental methods
    Introduce CollectDataProcess class in PersonalWebApi.Processes.NopCommerce.Processes namespace. Add CreateProcess and PrepareKelner methods, both marked with [Experimental("SKEXP0080")]. CreateProcess sets up a "CollectData" process using ProcessBuilder, while PrepareKelner configures a Kernel object with OpenAI settings and various services. Add necessary namespaces for new functionality.

---
+++ Processes/NopCommerce/Processes/CollectDataProcess.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.SemanticKernel;
+using nopCommerceApiHub.WebApi;
+using nopCommerceApiHub.WebApi.Exceptions;
+using PersonalWebApi.Processes.FileStorage.Events;
+using PersonalWebApi.Processes.FileStorage.Steps;
+using PersonalWebApi.Processes.NopCommerce.Events;
+using PersonalWebApi.Processes.NopCommerce.Steps;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PersonalWebApi.Processes.NopCommerce.Processes
+{
+    public class CollectDataProcess
+    {
+
+        [Experimental("SKEXP0080")]
+        public static ProcessBuilder CreateProcess(string processName = "CollectData")
+        {
+            var process = new ProcessBuilder(processName);
+
+            var nopProductSteps = process.AddStepFromType<ProductNopStep>();
+
+            process.OnInputEvent(ProductNopEvents.StartProcess).SendEventTo(
+                new ProcessFunctionTargetBuilder(nopProductSteps, functionName: ProductNopFunctions.GetTitle, parameterName: "sku"));
+
+            return process;
+        }
+
+        [Experimental("SKEXP0080")]
+        public static Kernel PrepareKelner(IConfiguration configuration)
+        {
+            var kernelBuilder = Kernel.CreateBuilder();
+
+            var apiKey = configuration.GetSection("OpenAI:Access:ApiKey").Value ??
+                throw new SettingsException("OpenAi ApiKey not exists in appsettings");
+
+            var defaultModelId = configuration.GetSection("OpenAI:DefaultModelId").Value ??
+                throw new SettingsException("OpenAi DefaultModelId not exists in appsettings");
+
+            kernelBuilder.AddOpenAIChatCompletion(
+                defaultModelId,
+                apiKey
+            );
+
+            kernelBuilder.Services.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.AddConsole();
+            });
+
+            kernelBuilder.Services.AddSingleton<IConfiguration>(configuration);
+            kernelBuilder.Services.AddHttpContextAccessor();
+
+            kernelBuilder.Services.Configure<StolargoPLApiSettings>(configuration.GetSection("NopCommerceStolargoPLApiSettings"));
+            kernelBuilder.Services.Configure<StolargoPLTokentSettings>(configuration.GetSection("NopCommerceStolargoPLTokenSettings"));
+            kernelBuilder.Services.AddScoped<PersonalWebApi.Services.NopCommerce.NopCommerce>();
+
+            return kernelBuilder.Build();
+        }
+    }
+}
