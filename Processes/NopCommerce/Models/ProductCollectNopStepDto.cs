3fcaefa883185a47aa6476562057e6ebc28017f6
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-02-25 12:34:37 +0100

    Add ProductCollectNopStepDto class with required properties
    Introduced a new class `ProductCollectNopStepDto` in the
    `PersonalWebApi.Processes.NopCommerce.Models` namespace. This class
    includes three properties: `Sku` (required), `Product`, and `Category`.
    Added necessary using directives for `nopCommerceApiHub.WebApi.DTOs`
    and `System.ComponentModel.DataAnnotations`.

---
+++ Processes/NopCommerce/Models/ProductCollectNopStepDto.cs
@@ -0,0 +1,13 @@
+﻿using nopCommerceApiHub.WebApi.DTOs;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalWebApi.Processes.NopCommerce.Models
+{
+    public class ProductCollectNopStepDto
+    {
+        [Required]
+        public string Sku { get; set; }
+        public ProductDto Product { get; set; }
+        public CategoryDto Category { get; set; }
+    }
+}
