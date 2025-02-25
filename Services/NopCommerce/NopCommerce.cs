51ceb7d465b245dedeba0d8665d7b03d43729055
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-02-25 12:35:55 +0100

    Add NopCommerce service integration
    Introduce NopCommerce class in PersonalWebApi.Services.NopCommerce to interact with various NopCommerce services. Add necessary using directives for Microsoft.Extensions.Options, nopCommerceApiHub.ApiClient.Token, nopCommerceApiHub.WebApi, and nopCommerceApiHub.WebApi.Services. Include private fields for token and API settings management. Define properties for Product, Manufacturer, ProductManufacturerMapping, and DeliveryDate services. Implement constructor to initialize services and settings. Add private method getPageToken for token retrieval. Add public async method CheckConnectionAsync to verify API connection.

---
+++ Services/NopCommerce/NopCommerce.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Extensions.Options;
+using nopCommerceApiHub.ApiClient.Token;
+using nopCommerceApiHub.WebApi;
+using nopCommerceApiHub.WebApi.Services;
+
+namespace PersonalWebApi.Services.NopCommerce
+{
+    /// <summary>
+    /// All objects from Subiekt GT and Nopcommerce
+    /// </summary>
+    public class NopCommerce
+    {
+        #region NopCommerce
+
+        private readonly IToken _tokenPage;
+        private readonly IOptions<StolargoPLApiSettings> _stolargoPLApiSettingsOptions;
+        private readonly IOptionsMonitor<StolargoPLTokentSettings> _stolargoPLTokentSettings;
+
+        public ProductNopCommerceService Product { get; set; }
+        public ManufacturerNopCommerceService Manufacturer { get; set; }
+        public ProductManufaturerMappingNopCommerceService ProductManufaturerMapping { get; set; }
+        public DeliveryDateNopCommerceService DeliveryDate { get; set; }
+
+        #endregion
+
+        public NopCommerce(
+            IOptions<StolargoPLApiSettings> stolargoPLApiSettings,
+            IOptionsMonitor<StolargoPLTokentSettings> stolargoPLTokentSettings,
+            ILogger<NopCommerce> logger
+            )
+        {
+
+            _stolargoPLApiSettingsOptions = stolargoPLApiSettings;
+            _stolargoPLTokentSettings = stolargoPLTokentSettings;
+            _tokenPage = getPageToken();
+
+            NopCommerceStolargoPLService nopCommerceStolargoPLService = new NopCommerceStolargoPLService(_stolargoPLApiSettingsOptions.Value.Url, _stolargoPLApiSettingsOptions.Value.TokenEndPoint, _tokenPage, logger);
+
+            Product = new ProductNopCommerceService(nopCommerceStolargoPLService);
+            Manufacturer = new ManufacturerNopCommerceService(nopCommerceStolargoPLService);
+            ProductManufaturerMapping = new ProductManufaturerMappingNopCommerceService(nopCommerceStolargoPLService);
+            DeliveryDate = new DeliveryDateNopCommerceService(nopCommerceStolargoPLService);
+        }
+
+        private IToken getPageToken()
+        {
+            var tokenSettingsManager = new TokenSettingsManager(_stolargoPLTokentSettings);
+
+            var token = new Token(
+                _stolargoPLApiSettingsOptions.Value.Username,
+                _stolargoPLApiSettingsOptions.Value.Password,
+                tokenSettingsManager,
+                "NopCommerceStolargoPLTokenSettings");
+
+            return token;
+        }
+
+        // check connection to api
+        public async Task<bool> CheckConnectionAsync()
+        {
+            try
+            {
+                await DeliveryDate.GetAllAsync();
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
