f8dd8411e4c5050aa67b6c9c3bb6846882f162b1
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-02-25 12:34:25 +0100

    Add ProductNopEvents class with event constants
    Introduced a new namespace `PersonalWebApi.Processes.NopCommerce.Events`.
    Added `ProductNopEvents` class within the new namespace.
    Defined three constant string fields: `StartProcess`, `Readed`, and `Paraphrased`.
    These constants represent different states or events in a NopCommerce product process.

---
+++ Processes/NopCommerce/Events/ProductNopEvents.cs
@@ -0,0 +1,13 @@
+﻿namespace PersonalWebApi.Processes.NopCommerce.Events
+{
+    public class ProductNopEvents
+    {
+        // start paraphrase process
+        public const string StartProcess = nameof(StartProcess);
+
+        public const string Readed = nameof(Readed);
+
+        // end
+        public const string Paraphrased = nameof(Paraphrased);
+    }
+}
